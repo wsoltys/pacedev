e7aa75ff3a98e11db7f11cff68b009817b8dc9ea
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2008-01-11 08:08:51 +0000

    fixed build for fpgaarcade-invaders at least
    git-svn-id: https://svn.pacedev.net/repos/pace@13 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/synth/platform/invaders-fpgaarcade/p2/project_pkg.vhd
+++ sw/synth/platform/invaders-fpgaarcade/p2/project_pkg.vhd
@@ -12,21 +12,18 @@ package project_pkg is
 	--
 	
   -- Reference clock is 24MHz
-  constant PACE_HAS_PLL               : boolean := true;
-  constant PACE_CLK0_DIVIDE_BY        : natural := 12;
-  constant PACE_CLK0_MULTIPLY_BY      : natural := 5;  		-- 24*5/12 = 10MHz
-  constant PACE_CLK1_DIVIDE_BY        : natural := 6;
-  constant PACE_CLK1_MULTIPLY_BY      : natural := 5;  		-- 24*5/6 = 20MHz
+  constant PACE_HAS_PLL                 : boolean := true;
+  constant PACE_CLK0_DIVIDE_BY          : natural := 12;
+  constant PACE_CLK0_MULTIPLY_BY        : natural := 5;  		-- 24*5/12 = 10MHz
+  constant PACE_CLK1_DIVIDE_BY          : natural := 6;
+  constant PACE_CLK1_MULTIPLY_BY        : natural := 5;  		-- 24*5/6 = 20MHz
 
-	--constant PACE_VIDEO_H_SCALE         : integer := 2;
-	--constant PACE_VIDEO_V_SCALE         : integer := 2;
+	constant PACE_ENABLE_ADV724					  : std_logic := '0';
+	constant PACE_ADV724_STD						  : std_logic := ADV724_STD_PAL;
 
-	constant PACE_SRAM_DATA_WIDTH				: natural := 8;
-	
-	constant PACE_ENABLE_ADV724					: std_logic := '0';
-	constant PACE_ADV724_STD						: std_logic := ADV724_STD_PAL;
+  -- P2 constants which *MUST* be defined
 
-	-- Donkey Kong-specific constants
-	constant DKONG_INTERNAL_ROM					: boolean := true;
-				
+	constant P2_JAMMA_IS_MAPLE	          : boolean := false;
+	constant P2_JAMMA_IS_NGC              : boolean := true;
+
 end;
