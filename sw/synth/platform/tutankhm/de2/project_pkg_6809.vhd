0032fd03b1e65dcce170a652a6bd54c403ec89c4
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2008-01-16 12:18:40 +0000

    project for external 6809
    git-svn-id: https://svn.pacedev.net/repos/pace@19 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/synth/platform/tutankhm/de2/project_pkg_6809.vhd
@@ -0,0 +1,40 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+library work;
+use work.pace_pkg.all;
+
+package project_pkg is
+
+	--  
+	-- PACE constants which *MUST* be defined
+	--
+	
+  -- Reference clock is 50MHz
+	constant PACE_HAS_PLL								      : boolean := true;
+  constant PACE_CLK0_DIVIDE_BY              : natural := 5;
+  constant PACE_CLK0_MULTIPLY_BY            : natural := 3;   -- 50*3/5 = 30MHz
+  constant PACE_CLK1_DIVIDE_BY              : natural := 5;
+  constant PACE_CLK1_MULTIPLY_BY            : natural := 4;  	-- 50*4/5 = 40MHz
+
+	constant PACE_VIDEO_H_SCALE               : integer := 2;
+	constant PACE_VIDEO_V_SCALE               : integer := 2;
+
+	-- DE2 constants which *MUST* be defined
+	
+	constant DE2_JAMMA_IS_MAPLE	              : boolean := false;
+	constant DE2_JAMMA_IS_NGC                 : boolean := false;
+	
+	constant DE2_LCD_LINE2							      : string := " TUTANKHAM-VGA  ";
+		
+	-- Tutankham-specific constants
+
+	constant TUTANKHAM_VRAM_WIDTHAD						: natural := 15;
+	constant TUTANKHAM_ROMS_IN_SRAM						: boolean := true;			
+	constant TUTANKHAM_CPU_CLK_ENA_DIVIDE_BY	: natural := 20;
+	constant TUTANKHAM_1MHz_CLK0_COUNTS			  : natural := 30;
+	
+	constant USE_VIDEO_VBLANK_INTERRUPT 		  : boolean := false;
+	
+end;
