8b7c133e19ba3ee4d8cb7373267029c9bd52b3e6
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2008-01-22 03:28:11 +0000

    fixed DE2 inputs problems
    git-svn-id: https://svn.pacedev.net/repos/pace@26 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/synth/platform/pacman/de2/project_pkg.vhd
+++ sw/synth/platform/pacman/de2/project_pkg.vhd
@@ -22,10 +22,10 @@ package project_pkg is
 
 	-- DE2 constants which *MUST* be defined
 	
-	constant DE2_JAMMA_IS_MAPLE	              : boolean := true;
+	constant DE2_JAMMA_IS_MAPLE	              : boolean := false;
 	constant DE2_JAMMA_IS_NGC                 : boolean := false;
 
-	constant DE2_LCD_LINE2							      : string := " PACMAN-LCD-DC  ";
+	constant DE2_LCD_LINE2							      : string := "   PACMAN-LCD   ";
 		
 	-- Pacman-specific constants
 			
