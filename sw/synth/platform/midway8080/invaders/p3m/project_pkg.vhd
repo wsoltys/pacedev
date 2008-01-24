baa7de582c314c2494fd96ddd1c91c6e28ad55e4
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2008-01-24 03:43:46 +0000

    a couple of more cleanups
    git-svn-id: https://svn.pacedev.net/repos/pace@49 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/synth/platform/midway8080/invaders/p3m/project_pkg.vhd
+++ sw/synth/platform/midway8080/invaders/p3m/project_pkg.vhd
@@ -4,6 +4,7 @@ use ieee.std_logic_unsigned.all;
 
 library work;
 use work.pace_pkg.all;
+use work.video_controller_pkg.all;
 
 package project_pkg is
 
@@ -12,14 +13,15 @@ package project_pkg is
 	--
 	
   -- Reference clock is 24.576MHz
-	constant PACE_HAS_PLL								: boolean := true;
-  constant PACE_CLK0_DIVIDE_BY        : natural := 16;
-  constant PACE_CLK0_MULTIPLY_BY      : natural := 13;  -- 24.576*13/16 = 19.968MHz
-  constant PACE_CLK1_DIVIDE_BY        : natural := 29;
-  constant PACE_CLK1_MULTIPLY_BY      : natural := 13;  -- 24.576*13/29 = 11.016MHz
+	constant PACE_HAS_PLL								      : boolean := true;
 
-	constant PACE_VIDEO_H_SCALE       	: integer := 1;
-	constant PACE_VIDEO_V_SCALE       	: integer := 1;
+  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_240x320_60Hz;
+  constant PACE_CLK0_DIVIDE_BY              : natural := 16;
+  constant PACE_CLK0_MULTIPLY_BY            : natural := 13;  -- 24.576*13/16 = 19.968MHz
+  constant PACE_CLK1_DIVIDE_BY              : natural := 29;
+  constant PACE_CLK1_MULTIPLY_BY            : natural := 13;  -- 24.576*13/29 = 11.016MHz
+	constant PACE_VIDEO_H_SCALE       	      : integer := 1;
+	constant PACE_VIDEO_V_SCALE       	      : integer := 1;
 
 	-- Space Invaders-specific constants
 			
