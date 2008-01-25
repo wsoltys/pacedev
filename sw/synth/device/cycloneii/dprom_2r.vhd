f55801adff5b2b8e75eb0918d2e37a97005cdbb1
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2008-01-25 06:34:47 +0000

    re-generated using quartus v7.2 (no different really)
    git-svn-id: https://svn.pacedev.net/repos/pace@50 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/synth/device/cycloneii/dprom_2r.vhd
+++ sw/synth/device/cycloneii/dprom_2r.vhd
@@ -7,33 +7,36 @@ USE altera_mf.all;
 ENTITY dprom_2r IS
 	GENERIC
 	(
-		init_file		: string := "";
-		numwords_a	: natural;
-		widthad_a		: natural;
-		width_a			: natural := 8;
-		numwords_b	: natural;
-		widthad_b		: natural;
-		width_b			: natural := 8
+		INIT_FILE		: string := "";
+		NUMWORDS_A	: natural;
+		WIDTHAD_A		: natural;
+		WIDTH_A			: natural := 8;
+		NUMWORDS_B	: natural;
+		WIDTHAD_B		: natural;
+		WIDTH_B			: natural := 8
 	);
 	PORT
 	(
-		address_a		: IN STD_LOGIC_VECTOR (widthad_a-1 DOWNTO 0);
-		address_b		: IN STD_LOGIC_VECTOR (widthad_b-1 DOWNTO 0);
-		clock		: IN STD_LOGIC ;
-		q_a		: OUT STD_LOGIC_VECTOR (width_a-1 DOWNTO 0);
-		q_b		: OUT STD_LOGIC_VECTOR (width_b-1 DOWNTO 0)
+		address_a		: in std_logic_vector (WIDTHAD_A-1 downto 0);
+		address_b		: in std_logic_vector (WIDTHAD_B-1 downto 0);
+		clock				: in std_logic ;
+		q_a					: out std_logic_vector (WIDTH_A-1 downto 0);
+		q_b					: out std_logic_vector (WIDTH_B-1 downto 0)
 	);
 END dprom_2r;
 
+
 ARCHITECTURE SYN OF dprom_2r IS
 
-	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (width_a-1 DOWNTO 0);
-	SIGNAL sub_wire1	: STD_LOGIC_VECTOR (width_b-1 DOWNTO 0);
+	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (WIDTH_A-1 DOWNTO 0);
+	SIGNAL sub_wire1	: STD_LOGIC_VECTOR (WIDTH_B-1 DOWNTO 0);
 	SIGNAL sub_wire2	: STD_LOGIC ;
-	SIGNAL sub_wire3_bv	: BIT_VECTOR (width_a-1 DOWNTO 0);
-	SIGNAL sub_wire3	: STD_LOGIC_VECTOR (width_a-1 DOWNTO 0);
-	SIGNAL sub_wire4_bv	: BIT_VECTOR (width_b-1 DOWNTO 0);
-	SIGNAL sub_wire4	: STD_LOGIC_VECTOR (width_b-1 DOWNTO 0);
+	SIGNAL sub_wire3_bv	: BIT_VECTOR (WIDTH_A-1 DOWNTO 0);
+	SIGNAL sub_wire3	: STD_LOGIC_VECTOR (WIDTH_A-1 DOWNTO 0);
+	SIGNAL sub_wire4_bv	: BIT_VECTOR (WIDTH_B-1 DOWNTO 0);
+	SIGNAL sub_wire4	: STD_LOGIC_VECTOR (WIDTH_B-1 DOWNTO 0);
+
+
 
 	COMPONENT altsyncram
 	GENERIC (
@@ -55,7 +58,7 @@ ARCHITECTURE SYN OF dprom_2r IS
 		outdata_reg_a		: STRING;
 		outdata_reg_b		: STRING;
 		power_up_uninitialized		: STRING;
-		read_during_write_mode_mixed_ports		: STRING;
+		ram_block_type		: STRING;
 		widthad_a		: NATURAL;
 		widthad_b		: NATURAL;
 		width_a		: NATURAL;
@@ -68,23 +71,23 @@ ARCHITECTURE SYN OF dprom_2r IS
 			wren_a	: IN STD_LOGIC ;
 			wren_b	: IN STD_LOGIC ;
 			clock0	: IN STD_LOGIC ;
-			address_a	: IN STD_LOGIC_VECTOR (widthad_a-1 DOWNTO 0);
-			address_b	: IN STD_LOGIC_VECTOR (widthad_b-1 DOWNTO 0);
-			q_a	: OUT STD_LOGIC_VECTOR (width_a-1 DOWNTO 0);
-			q_b	: OUT STD_LOGIC_VECTOR (width_b-1 DOWNTO 0);
-			data_a	: IN STD_LOGIC_VECTOR (width_a-1 DOWNTO 0);
-			data_b	: IN STD_LOGIC_VECTOR (width_b-1 DOWNTO 0)
+			address_a	: IN STD_LOGIC_VECTOR (WIDTHAD_A-1 DOWNTO 0);
+			address_b	: IN STD_LOGIC_VECTOR (WIDTHAD_B-1 DOWNTO 0);
+			q_a	: OUT STD_LOGIC_VECTOR (WIDTH_A-1 DOWNTO 0);
+			q_b	: OUT STD_LOGIC_VECTOR (WIDTH_B-1 DOWNTO 0);
+			data_a	: IN STD_LOGIC_VECTOR (WIDTH_A-1 DOWNTO 0);
+			data_b	: IN STD_LOGIC_VECTOR (WIDTH_B-1 DOWNTO 0)
 	);
 	END COMPONENT;
 
 BEGIN
 	sub_wire2    <= '0';
-	sub_wire3_bv(width_a-1 DOWNTO 0) <= (others => '0');
+	sub_wire3_bv(WIDTH_A-1 DOWNTO 0) <= (others => '0');
 	sub_wire3    <= To_stdlogicvector(sub_wire3_bv);
-	sub_wire4_bv(width_b-1 DOWNTO 0) <= (others => '0');
+	sub_wire4_bv(WIDTH_B-1 DOWNTO 0) <= (others => '0');
 	sub_wire4    <= To_stdlogicvector(sub_wire4_bv);
-	q_a    <= sub_wire0(width_a-1 DOWNTO 0);
-	q_b    <= sub_wire1(width_b-1 DOWNTO 0);
+	q_a    <= sub_wire0(WIDTH_A-1 DOWNTO 0);
+	q_b    <= sub_wire1(WIDTH_B-1 DOWNTO 0);
 
 	altsyncram_component : altsyncram
 	GENERIC MAP (
@@ -94,23 +97,23 @@ BEGIN
 		clock_enable_output_a => "BYPASS",
 		clock_enable_output_b => "BYPASS",
 		indata_reg_b => "CLOCK0",
-		init_file => init_file,
+		init_file => INIT_FILE,
 		init_file_layout => "PORT_A",
 		intended_device_family => "Cyclone II",
 		lpm_type => "altsyncram",
-		numwords_a => numwords_a,
-		numwords_b => numwords_b,
+		numwords_a => NUMWORDS_A,
+		numwords_b => NUMWORDS_B,
 		operation_mode => "BIDIR_DUAL_PORT",
 		outdata_aclr_a => "NONE",
 		outdata_aclr_b => "NONE",
 		outdata_reg_a => "UNREGISTERED",
 		outdata_reg_b => "UNREGISTERED",
 		power_up_uninitialized => "FALSE",
-		read_during_write_mode_mixed_ports => "DONT_CARE",
-		widthad_a => widthad_a,
-		widthad_b => widthad_b,
-		width_a => width_a,
-		width_b => width_b,
+		ram_block_type => "M4K",
+		widthad_a => WIDTHAD_A,
+		widthad_b => WIDTHAD_B,
+		width_a => WIDTH_A,
+		width_b => WIDTH_B,
 		width_byteena_a => 1,
 		width_byteena_b => 1,
 		wrcontrol_wraddress_reg_b => "CLOCK0"
