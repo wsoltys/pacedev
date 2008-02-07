7a3ce12ea64c780bbbaeca057a0c41e41f23bd95
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2008-02-07 01:50:54 +0000

    temp checkin before move
    git-svn-id: https://svn.pacedev.net/repos/pace@56 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/synth/platform/bbcmicro/project_pkg.vhd
+++ sw/synth/platform/bbcmicro/project_pkg.vhd
@@ -5,7 +5,6 @@ use ieee.std_logic_unsigned.all;
 library work;
 use work.pace_pkg.all;
 use work.target_pkg.all;
-use work.video_controller_pkg.all;
 
 package project_pkg is
 
@@ -16,7 +15,6 @@ package project_pkg is
   -- Reference clock is 24MHz
 	constant PACE_HAS_PLL										: boolean := true;
 
-  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_800x600_60Hz;
   constant PACE_CLK0_DIVIDE_BY        		  : natural := 1;
   constant PACE_CLK0_MULTIPLY_BY      		  : natural := 1;   -- 24*1/1 = 24MHz
   constant PACE_CLK1_DIVIDE_BY        		  : natural := 3;
