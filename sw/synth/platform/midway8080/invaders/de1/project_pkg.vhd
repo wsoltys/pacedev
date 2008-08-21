58188f65bf5105f7b6f055dd0d50afe41684275e
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2008-08-21 15:51:13 +0000

    works!
    git-svn-id: https://svn.pacedev.net/repos/pace@165 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/synth/platform/midway8080/invaders/de1/project_pkg.vhd
@@ -0,0 +1,43 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+library work;
+use work.pace_pkg.all;
+use work.video_controller_pkg.all;
+
+package project_pkg is
+
+	--  
+	-- PACE constants which *MUST* be defined
+	--
+	
+	constant PACE_HAS_PLL								      : boolean := true;	
+
+  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_800x600_60Hz;
+  constant PACE_CLK0_DIVIDE_BY              : natural := 5;
+  constant PACE_CLK0_MULTIPLY_BY            : natural := 2;   -- 50*2/5 = 20MHz
+  constant PACE_CLK1_DIVIDE_BY              : natural := 5;
+  constant PACE_CLK1_MULTIPLY_BY            : natural := 4;   -- 50*4/5 = 40MHz
+
+	constant PACE_VIDEO_H_SCALE               : integer := 2;
+	constant PACE_VIDEO_V_SCALE               : integer := 2;
+
+  constant PACE_HAS_OSD                     : boolean := false;
+  constant PACE_OSD_XPOS                    : natural := 0;
+  constant PACE_OSD_YPOS                    : natural := 0;
+
+	-- DE2 constants which *MUST* be defined
+	
+	constant DE1_JAMMA_IS_MAPLE	              : boolean := false;
+	constant DE1_JAMMA_IS_NGC                 : boolean := false;
+
+	-- Invaders-specific constants
+	
+	constant INVADERS_CPU_CLK_ENA_DIVIDE_BY   : natural := 10;
+	constant INVADERS_1MHz_CLK0_COUNTS		  : natural := 20;
+
+	constant INVADERS_USE_INTERNAL_WRAM       : boolean := true;		
+	constant USE_VIDEO_VBLANK_INTERRUPT       : boolean := false;
+	
+end;
