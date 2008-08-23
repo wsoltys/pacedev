0f08c7fedb45b78cbdb393c04c7b8c7de0a7ebcb
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2008-08-23 08:04:14 +0000

    no CVBS support on DE1 - doh!
    git-svn-id: https://svn.pacedev.net/repos/pace@179 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/synth/platform/videopac/de1/project_pkg.vhd
@@ -0,0 +1,33 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+library work;
+use work.pace_pkg.all;
+use work.target_pkg.all;
+
+package project_pkg is
+
+	--  
+	-- PACE constants which *MUST* be defined
+	--
+	
+	constant PACE_HAS_PLL								: boolean := false;
+	
+  -- Reference clock is 24MHz
+  constant PACE_CLK0_DIVIDE_BY        : natural := 21;
+  constant PACE_CLK0_MULTIPLY_BY      : natural := 31;  		-- 24*31/21 = 35.429MHz (want 35.469)
+  constant PACE_CLK1_DIVIDE_BY        : natural := 21;
+  constant PACE_CLK1_MULTIPLY_BY      : natural := 31;  		-- (not used)
+
+	constant PACE_ENABLE_ADV724					: std_logic := '1';
+	constant PACE_ADV724_STD						: std_logic := ADV724_STD_PAL;
+
+  -- P2-specific constants
+  constant P2_JAMMA_IS_MAPLE          : boolean := false;
+  constant P2_JAMMA_IS_NGC            : boolean := true;
+
+	-- Videopac-specific constants
+	constant VIDEOPAC_CART_NAME					: string := "kcmunch.hex";
+					
+end;
