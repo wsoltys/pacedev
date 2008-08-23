62290c088137c3a3f8acb5a87180cef8a8127b58
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2008-08-23 01:15:07 +0000

    git-svn-id: https://svn.pacedev.net/repos/pace@171 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/synth/platform/ladybug/cavenger/de1/project_pkg.vhd
@@ -0,0 +1,34 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+library work;
+use work.pace_pkg.all;
+use work.target_pkg.all;
+
+package project_pkg is
+
+	--  
+	-- PACE constants which *MUST* be defined
+	--
+	
+  -- Reference clock is 50MHz
+	-- LADYBUG wants 20MHz
+	constant PACE_HAS_PLL               : boolean := true;
+  constant PACE_CLK0_DIVIDE_BY        : natural := 5;
+  constant PACE_CLK0_MULTIPLY_BY      : natural := 2;  		-- 50*2/5 = 20MHz
+  constant PACE_CLK1_DIVIDE_BY        : natural := 1;
+  constant PACE_CLK1_MULTIPLY_BY      : natural := 1;  		-- 20MHz
+
+  -- DE1-specific constants
+  constant DE1_JAMMA_IS_MAPLE         : boolean := false;
+  constant DE1_JAMMA_IS_NGC           : boolean := true;
+
+	-- Ladybug-specific constants
+
+	constant LADYBUG_EXTERNAL_RAM				: integer := 0;
+				
+	constant LADYBUG_VIDEO_CVBS					: std_logic := '0';
+	constant LADYBUG_VIDEO_VGA					: std_logic := not LADYBUG_VIDEO_CVBS;
+
+end;
