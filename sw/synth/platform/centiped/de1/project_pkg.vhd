d2c0feb5c1c750b2f1b22508f85dafb9d4a5352f
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2008-08-23 00:57:57 +0000

    git-svn-id: https://svn.pacedev.net/repos/pace@170 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/synth/platform/centiped/de1/project_pkg.vhd
@@ -0,0 +1,40 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+library work;
+use work.pace_pkg.all;
+use work.target_pkg.all;
+use work.video_controller_pkg.all;
+
+package project_pkg is
+
+	--  
+	-- PACE constants which *MUST* be defined
+	--
+	
+  -- Reference clock is 50MHz
+	constant PACE_HAS_PLL								      : boolean := true;
+
+  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_800x600_60Hz;
+  constant PACE_CLK0_DIVIDE_BY              : natural := 5;
+  constant PACE_CLK0_MULTIPLY_BY            : natural := 3;   -- 50*3/5 = 30MHz
+  constant PACE_CLK1_DIVIDE_BY              : natural := 5;
+  constant PACE_CLK1_MULTIPLY_BY            : natural := 4;  	-- 50*4/5 = 40MHz
+	constant PACE_VIDEO_H_SCALE               : integer := 2;
+	constant PACE_VIDEO_V_SCALE               : integer := 2;
+	constant PACE_ENABLE_ADV724					      : std_logic := '0';
+	
+  constant PACE_HAS_OSD                     : boolean := false;
+  constant PACE_OSD_XPOS                    : natural := 0;
+  constant PACE_OSD_YPOS                    : natural := 0;
+
+  -- DE1-specific constants
+  constant DE1_JAMMA_IS_MAPLE               : boolean := false;
+  constant DE1_JAMMA_IS_NGC                 : boolean := true;
+
+	-- Centipede-specific constants
+
+	constant USE_VIDEO_VBLANK_INTERRUPT       : boolean := true;
+	
+end;
