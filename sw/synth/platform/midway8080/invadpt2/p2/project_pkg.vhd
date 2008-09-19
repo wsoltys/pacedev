33eddd935c24817eabb5941962a8457aefccd624
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2008-09-19 00:47:56 +0000

    updated midway8080 P2 projects (not tested)
    git-svn-id: https://svn.pacedev.net/repos/pace@241 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/synth/platform/midway8080/invadpt2/p2/project_pkg.vhd
+++ sw/synth/platform/midway8080/invadpt2/p2/project_pkg.vhd
@@ -15,7 +15,24 @@ package project_pkg is
 	
   -- Reference clock is 24MHz
 	constant PACE_HAS_PLL								      : boolean := true;
+  constant PACE_HAS_SDRAM                   : boolean := false;
+  constant PACE_HAS_SERIAL                  : boolean := false;
 	
+	constant PACE_JAMMA	                      : PACEJamma_t := PACE_JAMMA_NONE;
+	
+  ---- * defined in platform_pkg
+	--constant PACE_VIDEO_H_SIZE				        : integer := 224;
+	--constant PACE_VIDEO_V_SIZE				        : integer := 256; -- why not 240?
+
+  --constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_640x480_60Hz;
+  --constant PACE_CLK0_DIVIDE_BY              : natural := 6;
+  --constant PACE_CLK0_MULTIPLY_BY            : natural := 5;   -- 24*5/6 = 20MHz
+  --constant PACE_CLK1_DIVIDE_BY              : natural := 19;
+  --constant PACE_CLK1_MULTIPLY_BY            : natural := 20; 	-- 24*20/19 = 25.263158MHz
+	--constant PACE_VIDEO_H_SCALE       	      : integer := 1;
+	--constant PACE_VIDEO_V_SCALE       	      : integer := 1;
+  --constant PACE_ENABLE_ADV724					      : std_logic := '0';
+
   constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_800x600_60Hz;
   constant PACE_CLK0_DIVIDE_BY              : natural := 1;
   constant PACE_CLK0_MULTIPLY_BY            : natural := 1;   -- 24*1/1 = 24MHz
@@ -34,24 +51,21 @@ package project_pkg is
   --constant PACE_VIDEO_V_SCALE       	      : integer := 1;
   --constant PACE_ENABLE_ADV724					      : std_logic := '1';
 
+  --constant PACE_VIDEO_BORDER_RGB            : RGB_t := RGB_BLUE;
+  constant PACE_VIDEO_BORDER_RGB            : RGB_t := RGB_BLACK;
+
   constant PACE_HAS_OSD                     : boolean := false;
   constant PACE_OSD_XPOS                    : natural := 0;
   constant PACE_OSD_YPOS                    : natural := 0;
 
 	constant PACE_ADV724_STD						      : std_logic := ADV724_STD_PAL;
 
-  -- P2-specific constants
-  constant P2_JAMMA_IS_MAPLE          			: boolean := false;
-  constant P2_JAMMA_IS_NGC            			: boolean := true;
-
 	-- Space Invaders Pt.2-specific constants
 			
-	constant INVADERS_CPU_CLK_ENA_DIVIDE_BY		: natural := 12;
-	constant INVADERS_1MHz_CLK0_COUNTS				: natural := 24;
-	--constant INVADERS_CPU_CLK_ENA_DIVIDE_BY		: natural := 10;
-	--constant INVADERS_1MHz_CLK0_COUNTS				: natural := 20;
-
+  constant INVADERS_ROM_IN_FLASH            : boolean := false;
 	constant INVADERS_USE_INTERNAL_WRAM				: boolean := true;
+  constant PACE_HAS_SRAM                    : boolean := INVADERS_USE_INTERNAL_WRAM;
+
 	constant USE_VIDEO_VBLANK_INTERRUPT 			: boolean := false;
 	
 end;
