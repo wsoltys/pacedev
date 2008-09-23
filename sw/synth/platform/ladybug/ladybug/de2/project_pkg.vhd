513ab6fd50e602130866da7f162fccad2136dfcd
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2008-09-23 13:04:40 +0000

    DE2 ladybug projects synthesized
    git-svn-id: https://svn.pacedev.net/repos/pace@265 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/synth/platform/ladybug/ladybug/de2/project_pkg.vhd
+++ sw/synth/platform/ladybug/ladybug/de2/project_pkg.vhd
@@ -11,20 +11,21 @@ package project_pkg is
 	-- PACE constants which *MUST* be defined
 	--
 
-	constant PACE_HAS_PLL								      : boolean := true;	
+	constant PACE_HAS_PLL								      : boolean := true;
+  constant PACE_HAS_FLASH                   : boolean := false;
+  constant PACE_HAS_SRAM                    : boolean := true;
+  constant PACE_HAS_SDRAM                   : boolean := false;
+  constant PACE_HAS_SERIAL                  : boolean := false;
+	
+	constant PACE_JAMMA	                      : PACEJamma_t := PACE_JAMMA_NONE;
+
   constant PACE_CLK0_DIVIDE_BY              : natural := 5;
   constant PACE_CLK0_MULTIPLY_BY            : natural := 2;   -- 50*2/5 = 20MHz
   constant PACE_CLK1_DIVIDE_BY              : natural := 25;
   constant PACE_CLK1_MULTIPLY_BY            : natural := 12;  -- 50*12/25 = 24MHz
 
-	constant PACE_VIDEO_H_SCALE       	      : integer := 1;
-	constant PACE_VIDEO_V_SCALE       	      : integer := 1;
-
 	-- DE2 constants which *MUST* be defined
 	
-	constant DE2_JAMMA_IS_MAPLE	              : boolean := false;
-	constant DE2_JAMMA_IS_NGC                 : boolean := false;
-
 	constant DE2_LCD_LINE2							      : string := "  LADYBUG-VGA   ";
 		
 	-- Ladybug-specific constants
