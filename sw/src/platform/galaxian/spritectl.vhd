57058d5cfe30dd4680ec7bb388546b8e84f4998b
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2008-09-29 08:24:48 +0000

    tweaking sprites (WIP)
    git-svn-id: https://svn.pacedev.net/repos/pace@283 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/src/platform/galaxian/spritectl.vhd
+++ sw/src/platform/galaxian/spritectl.vhd
@@ -38,44 +38,49 @@ architecture SYN of spritectl is
   
   signal flipData : std_logic_vector(31 downto 0);   -- flipped row data
    
+  signal rgb      : RGB_t;
+  signal set      : std_logic;
+  
 begin
-
-  -- call up the flipper
-  --FLA : entity work.flipRow port map (rowIn => ctl_i.d, flip => reg_o.xflip, rowOut => flipData);
 
   flipData <= flip_row (ctl_i.d, reg_o.xflip);
   
 	process (clk, clk_ena)
 
    	variable rowStore : std_logic_vector(31 downto 0);  -- saved row of spt to show during visibile period
-		alias pel : std_logic_vector(1 downto 0) is rowStore(31 downto 30);
-    variable yMat  : boolean;                         	-- raster is between first and last line of sprite
-    variable xMat  : boolean;                         	-- raster in between left edge and end of line
-    variable xLocAdj : std_logic_vector(7 downto 0);
-    variable yLocAdj : std_logic_vector(8 downto 0);
+		--alias pel         : std_logic_vector(1 downto 0) is rowStore(rowStore'left downto rowStore'left-1);
+		variable pel      : std_logic_vector(1 downto 0);
+    variable x        : std_logic_vector(video_ctl.x'range);
+    variable y        : std_logic_vector(video_ctl.y'range);
+    variable yMat     : boolean;      -- raster is between first and last line of sprite
+    variable xMat     : boolean;      -- raster in between left edge and end of line
 
 		-- the width of rowCount determines the scanline multipler
 		-- - eg.	(4 downto 0) is 1:1
 		-- 				(5 downto 0) is 2:1 (scan-doubling)
   	variable rowCount : std_logic_vector(3+PACE_VIDEO_V_SCALE downto 0);
 
-		variable pal_i : std_logic_vector(3 downto 0);
-		variable pal_entry : pal_entry_typ;
+		variable pal_entry  : pal_entry_typ;
+
+    -- delay pipeline to match tilemap delay
+    type RGB_a_t is array (natural range <>) of RGB_t;
+    variable rgb_r      : RGB_a_t(DELAY-3 downto 0);
+    variable set_r      : std_logic_vector(DELAY-3 downto 0);
 
   begin
 
 		if rising_edge(clk) and clk_ena = '1' then
 
 			-- different offsets for sprites & bullets/bombs
+      --x := reg_o.x;
+			x := reg_o.x - (256-PACE_VIDEO_H_SIZE)/2;
 			if INDEX < 8 then
-				xLocAdj := reg_o.x(7 downto 0);
-	  		yLocAdj := reg_o.y(8 downto 0) + 1;
+	  		y := reg_o.y + 1;
 			else
-				xLocAdj := reg_o.x(7 downto 0) + 1;
-		  	yLocAdj := reg_o.y(8 downto 0) - 5;
+				--xLocAdj := reg_o.x(7 downto 0) + 1;
+		  	y := reg_o.y - 5;
 			end if;
 			-- video is clipped left and right (only 224 wide)
-			xLocAdj := xLocAdj - (256-PACE_VIDEO_H_SIZE)/2;
 			
 			if video_ctl.hblank = '1' then
 
@@ -85,7 +90,7 @@ begin
 					yMat := false;
 				end if;
 				
-				if yLocAdj = video_ctl.y then
+				if y = video_ctl.y then
 					-- start counting sprite row
 					rowCount := (others => '0');
 					yMat := true;
@@ -95,13 +100,13 @@ begin
 
 				-- sprites not visible before row 16				
 				if ctl_i.ld = '1' then
-					if yMat and yLocAdj > 16 then
+					if yMat and y > 16 then
 						if INDEX < 8 then
 							rowStore := flipData;			-- load sprite data
 						else
 							-- bullet/bomb sprite
 							if rowCount(rowCount'left downto rowCount'left-4) < 4 then
-								rowStore := ('1', '1', others => '0');
+								rowStore := (31=>'0', 30=>'0', 29=>'1', 28=>'1', others => '0');
 							else
 								rowStore := (others => '0');
 							end if;
@@ -113,7 +118,7 @@ begin
 						
 			else
 			
-				if video_ctl.x = xLocAdj then
+				if video_ctl.x = x then
 					-- count up at left edge of sprite
 					rowCount := rowCount + 1;
 					-- start of sprite
@@ -122,38 +127,51 @@ begin
 					end if;
 				end if;
 				
+				if video_ctl.stb = '1' and xMat then
+					-- shift in next pixel
+					pel := rowStore(rowStore'left downto rowStore'left-pel'length+1);
+					rowStore := rowStore(rowStore'left-2 downto 0) & "00";
+				end if;
+
+        -- shift the pipeline
+        rgb_r(rgb_r'left downto 1) := rgb_r(rgb_r'left-1 downto 0);
+        set_r(set_r'left downto 1) := set_r(set_r'left-1 downto 0);
+        
 				-- extract R,G,B from colour palette
 				-- apparently only 3 bits of colour info (aside from pel)
 				pal_entry := pal(conv_integer(reg_o.colour(2 downto 0) & pel));
-				ctl_o.rgb.r <= pal_entry(0) & "0000";
-				ctl_o.rgb.g <= pal_entry(1) & "0000";
-				ctl_o.rgb.b <= pal_entry(2) & "0000";
+				rgb_r(0).r(rgb_r(0).r'left downto rgb_r(0).r'left-5) := pal_entry(0);
+				rgb_r(0).r(rgb_r(0).r'left-6 downto 0) := (others => '0');
+				rgb_r(0).g(rgb_r(0).g'left downto rgb_r(0).g'left-5) := pal_entry(1);
+				rgb_r(0).g(rgb_r(0).g'left-6 downto 0) := (others => '0');
+				rgb_r(0).b(rgb_r(0).b'left downto rgb_r(0).b'left-5) := pal_entry(2);
+				rgb_r(0).b(rgb_r(0).b'left-6 downto 0) := (others => '0');
 
 			  -- set pixel transparency based on match
-				ctl_o.set <= '0';
+				set_r(0) := '0';
 				--if xMat and pel /= "00" then
 				if xMat and yMat and (pal_entry(0)(5 downto 4) /= "00" or
 															pal_entry(1)(5 downto 4) /= "00" or
 															pal_entry(2)(5 downto 4) /= "00") then
-			  	ctl_o.set <= '1';
-				end if;
-
-				if xMat then
-					-- shift in next pixel
-					rowStore := rowStore(29 downto 0) & "00";
+			  	set_r(0) := '1';
 				end if;
 
 			end if;
 
 		end if;
 
-	  ctl_o.a(15 downto 4) <= reg_o.n;
-	  if reg_o.yflip = '1' then
-	  	ctl_o.a(3 downto 0) <= not rowCount(rowCount'left-1 downto rowCount'left-4);		-- flip Y
-	  else
-	  	ctl_o.a(3 downto 0) <= rowCount(rowCount'left-1 downto rowCount'left-4);
-	  end if;
+    -- generate sprite data address
+    ctl_o.a(15 downto 4) <= reg_o.n;
+    if reg_o.yflip = '1' then
+      ctl_o.a(3 downto 0) <=  not rowCount(rowCount'left-1 downto rowCount'left-4);
+    else
+      ctl_o.a(3 downto 0) <= rowCount(rowCount'left-1 downto rowCount'left-4);
+    end if;
 
+    -- assign pipelined output
+    ctl_o.rgb <= rgb_r(rgb_r'left);
+    ctl_o.set <= set_r(set_r'left);
+    
   end process;
 
 end architecture SYN;
