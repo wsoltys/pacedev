0fa30877bf71f693023fdadb6831fc74bf53c7f9
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2008-11-07 02:59:57 +0000

    de2 frogger builds
    git-svn-id: https://svn.pacedev.net/repos/pace@375 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/synth/platform/frogger/de2/project_pkg.vhd
+++ sw/synth/platform/frogger/de2/project_pkg.vhd
@@ -12,15 +12,31 @@ package project_pkg is
 	-- PACE constants which *MUST* be defined
 	--
 	
-	constant PACE_HAS_PLL								      : boolean := true;	
+	constant PACE_HAS_PLL								      : boolean := true;
+  --constant PACE_HAS_SRAM                    : boolean := false;
+  constant PACE_HAS_SDRAM                   : boolean := false;
+  constant PACE_HAS_FLASH                   : boolean := false;
+  constant PACE_HAS_SERIAL                  : boolean := false;
+  
+	constant PACE_JAMMA	                      : PACEJamma_t := PACE_JAMMA_NONE;
 	
-  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_LCM_320x240_60Hz;
-  constant PACE_CLK0_DIVIDE_BY        			: natural := 5;
-  constant PACE_CLK0_MULTIPLY_BY      			: natural := 3;   -- 50*3/5 = 30MHz
-  constant PACE_CLK1_DIVIDE_BY        			: natural := 5;
-  constant PACE_CLK1_MULTIPLY_BY      			: natural := 4;   -- 50*9/25 = 18MHz
-	constant PACE_VIDEO_H_SCALE       				: integer := 1;
-	constant PACE_VIDEO_V_SCALE       				: integer := 1;
+  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_800x600_60Hz;
+  constant PACE_CLK0_DIVIDE_BY              : natural := 5;
+  constant PACE_CLK0_MULTIPLY_BY            : natural := 3;   -- 50*3/5 = 30MHz
+  constant PACE_CLK1_DIVIDE_BY              : natural := 5;
+  constant PACE_CLK1_MULTIPLY_BY            : natural := 4;  	-- 50*4/5 = 40MHz
+	constant PACE_VIDEO_H_SCALE               : integer := 2;
+	constant PACE_VIDEO_V_SCALE               : integer := 2;
+
+  --constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_LCM_320x240_60Hz;
+  --constant PACE_CLK0_DIVIDE_BY        			: natural := 5;
+  --constant PACE_CLK0_MULTIPLY_BY      			: natural := 3;   -- 50*3/5 = 30MHz
+  --constant PACE_CLK1_DIVIDE_BY        			: natural := 5;
+  --constant PACE_CLK1_MULTIPLY_BY      			: natural := 4;   -- 50*9/25 = 18MHz
+	--constant PACE_VIDEO_H_SCALE       				: integer := 1;
+	--constant PACE_VIDEO_V_SCALE       				: integer := 1;
+
+  constant PACE_VIDEO_BORDER_RGB            : RGB_t := RGB_GREEN;
 
   constant PACE_HAS_OSD                     : boolean := false;
   constant PACE_OSD_XPOS                    : natural := 0;
@@ -28,17 +44,15 @@ package project_pkg is
 
 	-- DE2 constants which *MUST* be defined
 	
-	constant DE2_JAMMA_IS_MAPLE	              : boolean := false;
-	constant DE2_JAMMA_IS_NGC                 : boolean := true;
-
-	constant DE2_LCD_LINE2							      : string := "  FROGGER-LCD   ";
+	constant DE2_LCD_LINE2							      : string := "  FROGGER-VGA   ";
 		
 	-- Frogger-specific constants
-	constant FROGGER_CPU_CLK_ENA_DIVIDE_BY		: natural := 10;
-	constant FROGGER_1MHz_CLK0_COUNTS					: natural := 30;
-	alias GALAXIAN_1MHz_CLK0_COUNTS				    : natural is FROGGER_1MHz_CLK0_COUNTS;
 
-  constant FROGGER_USE_INTERNAL_WRAM        : boolean := true;
-	constant USE_VIDEO_VBLANK_INTERRUPT 			: boolean := false;
+  constant FROGGER_HAS_SOUND                : boolean := false;
+	constant FROGGER_USE_INTERNAL_WRAM				: boolean := true;
+	constant FROGGER_USE_VIDEO_VBLANK 			  : boolean := true;
 
+  -- (derived)
+  constant PACE_HAS_SRAM                    : boolean := not FROGGER_USE_INTERNAL_WRAM;
+	
 end;
