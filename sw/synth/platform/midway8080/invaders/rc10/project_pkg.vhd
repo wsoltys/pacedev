d3876dae409381f0d36d3ac33748e20bbb22690f
RedskullDC <RedskullDC@693b85d5-0402-4c10-a79c-edb74d372fca>
2008-12-07 07:11:50 +0000

    Project Files for Digilent Nexys2-1200
    git-svn-id: https://svn.pacedev.net/repos/pace@424 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/synth/platform/midway8080/invaders/rc10/project_pkg.vhd
+++ sw/synth/platform/midway8080/invaders/rc10/project_pkg.vhd
@@ -13,19 +13,24 @@ package project_pkg is
 	-- PACE constants which *MUST* be defined
 	--
 
-	constant PACE_HAS_PLL								      : boolean := true;
+	constant PACE_HAS_PLL			: boolean := true;
+	constant PACE_HAS_SRAM                  : boolean := false;
+	constant PACE_HAS_SDRAM                 : boolean := false;
+	constant PACE_HAS_SERIAL                : boolean := false;
+  	constant PACE_JAMMA			: PACEJamma_t := PACE_JAMMA_NONE;
+
 	
-  -- Reference clock is 48MHz
+  -- Reference clock is 50MHz
   constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_800x600_60Hz;
-  constant PACE_CLK0_DIVIDE_BY              : natural := 2;    -- 48/2.0 = 24MHz
-  constant PACE_CLK0_MULTIPLY_BY            : natural := 1;    -- (used by SI)
-  constant PACE_CLK1_DIVIDE_BY              : natural := 6;
-  constant PACE_CLK1_MULTIPLY_BY            : natural := 5;  	-- 48*5/6 = 40MHz
+  constant PACE_CLK0_DIVIDE_BY              : natural := 2;		--5;  50/2.5 = 20MHz
+  constant PACE_CLK0_MULTIPLY_BY            : natural := 1;   
+  constant PACE_CLK1_DIVIDE_BY              : natural := 5;
+  constant PACE_CLK1_MULTIPLY_BY            : natural := 4;   -- 50*4/5 = 40MHz
   constant PACE_VIDEO_H_SCALE       	      : integer := 2;
   constant PACE_VIDEO_V_SCALE       	      : integer := 2;
 
-  constant PACE_VIDEO_BORDER_RGB            : RGB_t := RGB_BLUE;
-  --constant PACE_VIDEO_BORDER_RGB            : RGB_t := RGB_BLACK;
+  --constant PACE_VIDEO_BORDER_RGB            : RGB_t := RGB_BLUE;
+  constant PACE_VIDEO_BORDER_RGB            : RGB_t := RGB_BLACK;
   
   constant PACE_HAS_OSD                     : boolean := false;
   constant PACE_OSD_XPOS                    : natural := 0;
