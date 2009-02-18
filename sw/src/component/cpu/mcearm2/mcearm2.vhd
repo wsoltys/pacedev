f96c2e66ce45e90c916260b378cc3b3a1f65e610
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2009-02-18 13:23:43 +0000

    does more than chris' 6809 core
    git-svn-id: https://svn.pacedev.net/repos/pace@455 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/src/component/cpu/mcearm2/mcearm2.vhd
+++ sw/src/component/cpu/mcearm2/mcearm2.vhd
@@ -104,25 +104,181 @@ architecture SYN of mce_arm2 is
   constant COND_NV    : std_logic_vector(31 downto 28) := X"F";
 
   -- types of instructions
-  constant INSTR_DATA_PROC          : std_logic_vector(27 downto 26) := "00";
-  constant INSTR_MULTIPLY           : std_logic_vector(27 downto 22) := "000000";
-  constant INSTR_SINGLE_DATA_XFER   : std_logic_vector(27 downto 26) := "01";
-  constant INSTR_BLOCK_XFER         : std_logic_vector(27 downto 25) := "100";
-  constant INSTR_BRANCH             : std_logic_vector(27 downto 25) := "101";
-  constant INSTR_COPROC_DATA_XFER   : std_logic_vector(27 downto 25) := "110";
-  constant INSTR_COPROC_DATA_OP     : std_logic_vector(27 downto 24) := "1110";
-  constant INSTR_COPROC_DATA_OP_4   : std_logic_vector(4 downto 4) := "0";
-  constant INSTR_COPROC_REG_XFER    : std_logic_vector(27 downto 24) := "1110";
-  constant INSTR_COPROC_REG_XFER_4  : std_logic_vector(4 downto 4) := "1";
-  constant INSTR_SW_INT             : std_logic_vector(27 downto 24) := "1111";
+  constant INSTR_DATA_PROC_SI       : std_logic_vector(27 downto 0) := "00---------------------0----";
+  constant INSTR_DATA_PROC_SR       : std_logic_vector(27 downto 0) := "00------------------0--1----";
+  constant INSTR_MULTIPLY           : std_logic_vector(27 downto 0) := "000000--------------1001----";
+  constant INSTR_SINGLE_DATA_XFER   : std_logic_vector(27 downto 0) := "01--------------------------";
+  constant INSTR_BLOCK_XFER         : std_logic_vector(27 downto 0) := "100-------------------------";
+  constant INSTR_BRANCH             : std_logic_vector(27 downto 0) := "101-------------------------";
+  constant INSTR_COPROC_DATA_XFER   : std_logic_vector(27 downto 0) := "110-------------------------";
+  constant INSTR_COPROC_DATA_OP     : std_logic_vector(27 downto 0) := "1110-------------------0----";
+  constant INSTR_COPROC_REG_XFER    : std_logic_vector(27 downto 0) := "1110-------------------1----";
+  constant INSTR_SW_INT             : std_logic_vector(27 downto 0) := "1111------------------------";
 
   -- internal logic
   signal din        : std_logic_vector(31 downto 0) := (others => '0');
   signal ar         : std_logic_vector(25 downto 0) := (others => '0');
 
+	type ExecuteMode_t is 
+	( 
+		first_step, refill1, refill2, store, load_write_register, multi_load_loop, multi_store_init, alu_shift,
+		load_read_memory 
+	);
+	signal ExecuteMode 	: ExecuteMode_t := refill2;
+
+	signal PrevFetchedInstr 	: std_logic_vector(31 downto 0) := (others => '0');
+	signal FetchedInstr 			: std_logic_vector(31 downto 0) := (others => '0');
+	signal ExecuteInstr				: std_logic_vector(31 downto 0) := (others => '0');
+	signal DestReg						: integer := 0;
+	signal aop								: std_logic_vector(31 downto 0) := (others => '0');
+	signal Bop								: std_logic_vector(31 downto 0) := (others => '0');
+	signal ShiftCarryOp				: std_logic := '0';
+	signal Status							: std_logic_vector(5 downto 0) := (others => '0');
+	signal DataIn							: std_logic_vector(31 downto 0) := (others => '0');
+
+	function Nop (instr : std_logic_vector(31 downto 0)) return boolean is
+	begin
+		-- quick fudge
+		return instr(31 downto 28) = COND_NV;
+	end function Nop;
+
+	function Satisfied (Status : std_logic_vector(5 downto 0); CondCode : std_logic_vector(3 downto 0)) return boolean is
+		variable retval : boolean := false;
+		alias N : std_logic is Status(5);
+		alias Z : std_logic is Status(4);
+		alias C : std_logic is Status(3);
+		alias V : std_logic is Status(2);
+		alias I : std_logic is Status(1);
+		alias F : std_logic is Status(0);
+	begin
+		-- NZCVIF
+		return
+			(CondCode = COND_EQ and Z = '1') or
+			(CondCode = COND_NE and Z = '0') or
+			(CondCode = COND_CS and C = '1') or
+			(CondCode = COND_CC and C = '0') or
+			(CondCode = COND_MI and N = '1') or
+			(CondCode = COND_PL and N = '0') or
+			(CondCode = COND_VS and V = '1') or
+			(CondCode = COND_VC and V = '0') or
+			(CondCode = COND_HI and C = '1' and Z = '0') or
+			(CondCode = COND_LS and C = '0' and Z = '1') or
+			(CondCode = COND_GE and ((N = '1' and V = '1') or (N = '1' and V = '0'))) or
+			(CondCode = COND_LT and ((N = '1' and V = '0') or (N = '0' and V = '1'))) or
+			(CondCode = COND_GT and Z = '0' and ((N = '1' and V = '1') or (N = '0' and V = '0'))) or
+			(CondCode = COND_LE and Z = '1' and ((N = '1' and V = '0') or (N = '0' and V = '1'))) or
+			CondCode = COND_AL;
+	end function Satisfied;
+
+	function CondCode (instr : std_logic_vector(31 downto 0)) return std_logic_vector is
+	begin
+		return instr(31 downto 28);
+	end function CondCode;
+
+	function LastStep (instr : std_logic_vector(31 downto 0); ExecuteMode : ExecuteMode_t) return boolean is
+	begin
+		-- quick fudge
+		return true;
+	end function LastStep;
+
+	function DestOp (cc_instr : std_logic_vector(31 downto 0)) return integer is
+		alias instr : std_logic_vector(27 downto 0) is cc_instr(27 downto 0);
+	begin
+		if STD_MATCH(instr, INSTR_DATA_PROC_SI) or STD_MATCH(instr, INSTR_DATA_PROC_SR) then
+			return to_integer(unsigned(instr(15 downto 12)));
+		elsif STD_MATCH(instr, INSTR_MULTIPLY) then
+			return to_integer(unsigned(instr(19 downto 16)));
+		elsif STD_MATCH(instr, INSTR_SINGLE_DATA_XFER) and instr(20) = '1' then
+			return to_integer(unsigned(instr(15 downto 12)));
+		else
+			return 0;
+		end if;
+	end function DestOp;
+
+	function AluRegShift (cc_instr : std_logic_vector(31 downto 0)) return boolean is
+		alias instr : std_logic_vector(27 downto 0) is cc_instr(27 downto 0);
+	begin
+		return 
+			(STD_MATCH(instr, INSTR_DATA_PROC_SI) and false) or
+			(STD_MATCH(instr, INSTR_DATA_PROC_SR) and false) or
+			(STD_MATCH(instr, INSTR_SINGLE_DATA_XFER) and false);
+	end function AluRegShift;
+
+	function AopReg (cc_instr : std_logic_vector(31 downto 0)) return integer is
+		alias instr : std_logic_vector(27 downto 0) is cc_instr(27 downto 0);
+	begin
+		if STD_MATCH(instr, INSTR_DATA_PROC_SI) or STD_MATCH(instr, INSTR_DATA_PROC_SR) then
+			return to_integer(unsigned(instr(19 downto 16)));
+		elsif false then
+			-- how to handle MULT???
+		else
+			return 0;
+		end if;
+	end function AopReg;
+
+	function AluInstr (cc_instr : std_logic_vector(31 downto 0)) return boolean is
+		alias instr : std_logic_vector(27 downto 0) is cc_instr(27 downto 0);
+	begin
+		return 
+			STD_MATCH(instr, INSTR_DATA_PROC_SI) or
+			STD_MATCH(instr, INSTR_DATA_PROC_SR) or
+			STD_MATCH(instr, INSTR_MULTIPLY);
+	end function AluInstr;
+
+	function WriteResult (cc_instr : std_logic_vector(31 downto 0)) return boolean is
+		alias instr : std_logic_vector(27 downto 0) is cc_instr(27 downto 0);
+	begin
+		return 
+			(STD_MATCH(instr, INSTR_DATA_PROC_SI) or STD_MATCH(instr, INSTR_DATA_PROC_SR)) and
+				-- TST, TEQ, CMP & CMN discard result
+				not STD_MATCH(instr(24 downto 21), "10--");
+	end function WriteResult;
+
+	function SetCondCode (cc_instr : std_logic_vector(31 downto 0)) return boolean is
+		alias instr : std_logic_vector(27 downto 0) is cc_instr(27 downto 0);
+	begin
+		return 
+			(STD_MATCH(instr, INSTR_DATA_PROC_SI) and instr(20) = '1') or
+			(STD_MATCH(instr, INSTR_DATA_PROC_SR) and instr(20) = '1') or
+			(STD_MATCH(instr, INSTR_MULTIPLY) and instr(20) = '1');
+	end function SetCondCode;
+
+	function WritesPC (cc_instr : std_logic_vector(31 downto 0)) return boolean is
+		alias instr : std_logic_vector(27 downto 0) is cc_instr(27 downto 0);
+	begin
+		return 
+			-- fixme
+			(STD_MATCH(instr, INSTR_BRANCH));
+	end function WritesPC;
+
+	function SingleLoadInstr (cc_instr : std_logic_vector(31 downto 0)) return boolean is
+		alias instr : std_logic_vector(27 downto 0) is cc_instr(27 downto 0);
+	begin
+		return STD_MATCH(instr, INSTR_SINGLE_DATA_XFER) and instr(20) = '1';
+	end function SingleLoadInstr;
+
+	function FinalOffset (instr : std_logic_vector(31 downto 0)) return std_logic_vector is
+	begin
+		return X"00000000";
+	end function FinalOffset;
+
+	function ByteTransferInstr (cc_instr : std_logic_vector(31 downto 0)) return boolean is
+		alias instr : std_logic_vector(27 downto 0) is cc_instr(27 downto 0);
+	begin
+		return STD_MATCH(instr, INSTR_SINGLE_DATA_XFER) and instr(22) = '1';
+	end function ByteTransferInstr;
+
+	function WriteBack (cc_instr : std_logic_vector(31 downto 0)) return boolean is
+		alias instr : std_logic_vector(27 downto 0) is cc_instr(27 downto 0);
+	begin
+		return 
+			(STD_MATCH(instr, INSTR_SINGLE_DATA_XFER) and instr(21) = '1') or
+			(STD_MATCH(instr, INSTR_BLOCK_XFER) and instr(21) = '1');
+	end function WriteBack;
+
 begin
 
-  process (clk_i, reset_i)
+  PROC_DELETEME : process (clk_i, reset_i)
     variable reset_r : std_logic := '0';
   begin
     if reset_i = '1' then
@@ -137,77 +293,206 @@ begin
         m <= SVC_MODE;
         f_i <= '1';       -- disable IRQ
         f_f <= '1';       -- disable FIQ
-        pc <= VEC_RESET;
       elsif ph1_ena = '1' then
-        pc <= std_logic_vector(unsigned(pc) + 1);
       elsif ph2_ena = '1' then
         null;
       end if;
       reset_r := reset;
     end if;
-  end process;
+  end process PROC_DELETEME;
 
   --
   -- FETCH
   --
 
-  process (clk_i, reset_i)
-  begin
-    if reset_i = '1' then
-    elsif reset = '1' then
-    elsif rising_edge(clk_i) and ph1_ena = '1' then
-      -- assume fetch always OK
-      rn_w <= '0';
-      din <= d_i;
-      a <= pc & "00";
-    end if;
-  end process;
+	BLK_FETCH : block
+	begin
+
+	  PROC_FETCH : process (clk_i, reset_i)
+			variable FetchOK				: boolean := false;
+			variable FetchInstr 		: std_logic_vector(31 downto 0) := (others => '0');
+
+	  begin
+
+			FetchOK := (ExecuteMode = first_step) or (ExecuteMode = refill1) or (ExecuteMode = refill2);
+			FetchInstr := d_i; -- hack??
+
+	    if reset_i = '1' then
+	    elsif reset = '1' then
+	    elsif rising_edge(clk_i) and ph1_ena = '1' then
+	      if FetchOK then
+					FetchedInstr <= FetchInstr;
+					PrevFetchedInstr <= FetchedInstr;
+				end if;
+	    end if;
+	  end process PROC_FETCH;
+
+    rn_w <= '0';
+    a <= pc & "00";
+
+	end block BLK_FETCH;
 
   --
   -- DECODE
   --
 
-  process (clk_i, reset_i)
-  begin
-    if reset_i = '1' then
-    elsif reset = '1' then
-    elsif rising_edge(clk_i) and ph1_ena = '1' then
-      -- assume decode always OK
-      -- - may be able to re-arrange order to optimize performance
-      if din(27 downto 22) = INSTR_MULTIPLY then
-      elsif din(27 downto 24) = INSTR_COPROC_DATA_OP then
-      elsif din(27 downto 24) = INSTR_COPROC_REG_XFER then
-      elsif din(27 downto 24) = INSTR_SW_INT then
-      elsif din(27 downto 25) = INSTR_BLOCK_XFER then
-      elsif din(27 downto 25) = INSTR_BRANCH then
-      elsif din(27 downto 25) = INSTR_COPROC_DATA_XFER then
-      elsif din(27 downto 26) = INSTR_DATA_PROC then
-      elsif din(27 downto 26) = INSTR_SINGLE_DATA_XFER then
-        ar <= "00000000000000" & din(11 downto 0);
-        if din(20) = '0' then
-        else
-        end if;
-      else
-        -- undefined
-      end if;
-    end if;
-  end process;
+	BLK_DECODE : block
+
+	begin
+
+	  PROC_DECODE : process (clk_i, reset_i)
+
+			variable DecodeOK 			: boolean := false;
+			variable DecodeInstr		: std_logic_vector(31 downto 0) := (others => '0');
+
+	  begin
+
+			-- quick hack
+			DecodeOK := Satisfied (Status, CondCode (ExecuteInstr)) or LastStep (ExecuteInstr, ExecuteMode);
+			if ExecuteMode = first_step or ExecuteMode = refill2 then
+				DecodeInstr := FetchedInstr;
+			else
+				DecodeInstr := PrevFetchedInstr;
+			end if;
+
+	    if reset_i = '1' then
+	    elsif reset = '1' then
+	    elsif rising_edge(clk_i) and ph1_ena = '1' then
+
+				if DecodeOK then
+					ExecuteInstr <= DecodeInstr;
+					if not Nop (DecodeInstr) then
+						DestReg <= DestOp (DecodeInstr);
+						if not AluRegShift (DecodeInstr) then
+							aop <= r(AopReg (DecodeInstr));
+						end if;
+						Bop <= (others => '0'); -- hack
+						ShiftCarryOp <= '0';    -- hack
+					end if;
+				end if;
+
+			end if;
+
+	  end process PROC_DECODE;
+
+	end block BLK_DECODE;
 
   --
   -- EXECUTE
   --
 
-  process (clk_i, reset_i)
-  begin
-    if reset_i = '1' then
-    elsif reset = '1' then
-    elsif rising_edge(clk_i) and ph1_ena = '1' then
-      -- assume execute always OK
-      case din(31 downto 28) is
-        when COND_NV =>
-        when others =>
-      end case;
-    end if;
-  end process;
+	BLK_EXECUTE : block
+
+	begin
+
+	  PROC_EXECUTE : process (clk_i, reset_i)
+
+			variable ExecuteOK		: boolean := false;
+
+	  begin
+
+			ExecuteOK := ExecuteMode = first_step;
+
+	    if reset_i = '1' then
+	    elsif reset = '1' then
+        pc <= VEC_RESET;
+				ExecuteMode <= refill2;
+	    elsif rising_edge(clk_i) and ph1_ena = '1' then
+
+				--
+				-- Rule: ExecutePC
+				--
+
+				if ExecuteOK then
+					if true then --cc or writes pc
+						pc <= std_logic_vector(unsigned(pc) + 1);
+					end if;
+				end if;
+
+				--
+				-- Rule: ExecuteALU
+				--
+
+				if ExecuteMode = first_step and AluInstr(ExecuteInstr) and not AluRegShift(ExecuteInstr) then
+					if true then -- CC
+						if WriteResult (ExecuteInstr) then
+							-- do it
+						end if;
+						if SetCondCode (ExecuteInstr) then
+							-- do it
+						end if;
+					end if;
+					if WritesPC (ExecuteInstr) then
+						ExecuteMode <= refill1;
+					else
+						ExecuteMode <= first_step;
+					end if;
+				end if;
+
+				--
+				-- Rule: ALU-RegisterShift
+				--
+
+				if ExecuteMode = first_step and AluRegShift (ExecuteInstr) then
+					if true then -- CC
+						aop <= r(AopReg (ExecuteInstr));
+						ExecuteMode <= alu_shift;
+					end if;
+				end if;
+
+				if ExecuteMode = alu_shift then
+					if WriteResult (ExecuteInstr) then
+						r(DestReg) <= (others => '1'); -- ALU
+					end if;
+					if SetCondCode (ExecuteInstr) then
+						Status <= (others => '0'); -- updatestatus
+					end if;
+					if WritesPC (ExecuteInstr) then
+						ExecuteMode <= refill1;
+					else
+						ExecuteMode <= first_step;
+					end if;
+				end if;
+
+				--
+				-- Rule: SingleLoad
+				--
+
+				if SingleLoadInstr (ExecuteInstr) then
+					if ExecuteMode = first_step and true then
+						--
+						ExecuteMode <= load_read_memory;
+						Bop <= FinalOffset(ExecuteInstr);
+					elsif ExecuteMode = load_read_memory then
+						if ByteTransferInstr (ExecuteInstr) then
+							DataIn <= (others => '1');
+						else
+							DataIn <= (others => '1');
+						end if;
+						if WriteBack (ExecuteInstr) then
+						end if;
+						ExecuteMode <= load_write_register;
+					elsif ExecuteMode = load_write_register then
+						r(DestReg) <= DataIn;
+						if WritesPC (ExecuteInstr) then
+							ExecuteMode <= refill1;
+						else
+							ExecuteMode <= first_step;
+						end if;
+					end if;
+				end if;
+
+				-- I made this up...
+				if ExecuteMode = refill2 then
+					ExecuteMode <= refill1;
+				elsif ExecuteMode = refill1 then
+					ExecuteMode <= first_step;
+				end if;
+
+	    end if; -- rising_edge()
+
+	  end process PROC_EXECUTE;
+
+	end block BLK_EXECUTE;
 
 end architecture SYN;
--- sw/src/component/cpu/mcearm2/tb/wave.do
+++ sw/src/component/cpu/mcearm2/tb/wave.do
@@ -12,11 +12,20 @@ add wave -noupdate -format Literal /tb_top/syn_cpu/d_i
 add wave -noupdate -format Literal -radix hexadecimal /tb_top/syn_cpu/pc
 add wave -noupdate -format Literal /tb_top/syn_cpu/m
 add wave -noupdate -divider FETCH
-add wave -noupdate -format Literal /tb_top/syn_cpu/din
-add wave -noupdate -format Literal /tb_top/syn_cpu/ar
+add wave -noupdate -format Logic /tb_top/syn_cpu/blk_fetch/proc_fetch/fetchok
+add wave -noupdate -format Literal /tb_top/syn_cpu/blk_fetch/proc_fetch/fetchinstr
+add wave -noupdate -format Literal /tb_top/syn_cpu/fetchedinstr
+add wave -noupdate -format Literal /tb_top/syn_cpu/prevfetchedinstr
+add wave -noupdate -divider DECODE
+add wave -noupdate -format Logic /tb_top/syn_cpu/blk_decode/proc_decode/decodeok
+add wave -noupdate -format Literal /tb_top/syn_cpu/blk_decode/proc_decode/decodeinstr
+add wave -noupdate -divider EXECUTE
+add wave -noupdate -format Literal /tb_top/syn_cpu/executemode
+add wave -noupdate -format Logic /tb_top/syn_cpu/blk_execute/proc_execute/executeok
+add wave -noupdate -format Literal /tb_top/syn_cpu/executeinstr
 TreeUpdate [SetDefaultTree]
 WaveRestoreCursors {{Cursor 1} {0 ps} 0}
-configure wave -namecolwidth 150
+configure wave -namecolwidth 340
 configure wave -valuecolwidth 100
 configure wave -justifyvalue left
 configure wave -signalnamewidth 0
@@ -29,4 +38,4 @@ configure wave -gridperiod 1
 configure wave -griddelta 40
 configure wave -timeline 0
 update
-WaveRestoreZoom {0 ps} {4200 ns}
+WaveRestoreZoom {0 ps} {4819030 ps}
