27ad9114974978e414ef574ea5870813f5784019
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2009-03-04 12:25:57 +0000

    wip
    git-svn-id: https://svn.pacedev.net/repos/pace@466 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/synth/platform/coco3-becker/p2a/project_pkg.vhd
@@ -0,0 +1,37 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+library work;
+use work.pace_pkg.all;
+use work.target_pkg.all;
+
+package project_pkg is
+
+	--  
+	-- PACE constants which *MUST* be defined
+	--
+	
+  -- Reference clock is 24MHz
+	constant PACE_HAS_PLL								      : boolean := true;
+  constant PACE_HAS_SRAM                    : boolean := true;
+  constant PACE_HAS_SDRAM                   : boolean := false;
+  constant PACE_HAS_SERIAL                  : boolean := true;
+	
+	constant PACE_JAMMA	                      : PACEJamma_t := PACE_JAMMA_NONE;
+  
+  constant PACE_CLK0_DIVIDE_BY        		  : natural := 12;
+  constant PACE_CLK0_MULTIPLY_BY      		  : natural := 25;  	-- 24*25/12 = 50MHz
+  constant PACE_CLK1_DIVIDE_BY        		  : natural := 12;
+  constant PACE_CLK1_MULTIPLY_BY      		  : natural := 25;  	-- 24*25/12 = 50MHz
+
+	constant PACE_ENABLE_ADV724							  : std_logic := '0';
+	constant PACE_ADV724_STD								  : std_logic := ADV724_STD_PAL;
+
+  -- P2-specific constants
+  constant P2_JAMMA_IS_MAPLE                : boolean := false;
+  constant P2_JAMMA_IS_NGC                  : boolean := true;
+
+	-- Coco3-specific constants
+
+end;
