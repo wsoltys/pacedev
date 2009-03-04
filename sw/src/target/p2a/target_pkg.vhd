176df9baeb29a939d39c0a4045dfd9ee75e55615
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2009-03-04 09:28:54 +0000

    for DLB A04
    git-svn-id: https://svn.pacedev.net/repos/pace@462 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/src/target/p2a/target_pkg.vhd
@@ -0,0 +1,26 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+library work;
+use work.pace_pkg.all;
+
+package target_pkg is
+
+	--  
+	-- PACE constants which *MUST* be defined
+	--
+	
+	constant PACE_TARGET 				: PACETargetType := PACE_TARGET_P2A;
+	constant PACE_FPGA_VENDOR		: PACEFpgaVendor_t := PACE_FPGA_VENDOR_ALTERA;
+	constant PACE_FPGA_FAMILY		: PACEFpgaFamily_t := PACE_FPGA_FAMILY_CYCLONE2;
+
+  constant PACE_CLKIN0        : natural := 24;
+  constant PACE_CLKIN1        : natural := 24;
+  constant PACE_HAS_FLASH     : boolean := false;
+
+	-- ADV724 constants
+	constant ADV724_STD_PAL		  : std_logic := '0';
+	constant ADV724_STD_NTSC	  : std_logic := not ADV724_STD_PAL;
+
+end;
