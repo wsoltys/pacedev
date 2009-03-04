37f7410a99a232d70f54aab819b42aeb4aa7abf5
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2009-03-04 15:15:01 +0000

    wip
    git-svn-id: https://svn.pacedev.net/repos/pace@468 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/synth/platform/coco3-becker/de1/project_pkg.vhd
@@ -0,0 +1,36 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+library work;
+use work.pace_pkg.all;
+use work.target_pkg.all;
+use work.video_controller_pkg.all;
+
+package project_pkg is
+
+	--  
+	-- PACE constants which *MUST* be defined
+	--
+	
+  -- Reference clock is 50MHz
+	constant PACE_HAS_PLL								      : boolean := true;
+  constant PACE_HAS_SRAM                    : boolean := true;
+  constant PACE_HAS_SDRAM                   : boolean := false;
+  constant PACE_HAS_SERIAL                  : boolean := true;
+
+	constant PACE_JAMMA	                      : PACEJamma_t := PACE_JAMMA_NONE;
+  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_NONE;
+
+  constant PACE_CLK0_DIVIDE_BY              : natural := 1;
+  constant PACE_CLK0_MULTIPLY_BY            : natural := 1;   -- 50*1/1 = 50MHz
+  constant PACE_CLK1_DIVIDE_BY              : natural := 1;
+  constant PACE_CLK1_MULTIPLY_BY            : natural := 1;  	-- 50*1/1 = 50MHz
+	constant PACE_VIDEO_H_SCALE       	      : integer := 1;
+	constant PACE_VIDEO_V_SCALE       	      : integer := 2;
+
+  constant PACE_HAS_OSD                     : boolean := false;
+  constant PACE_OSD_XPOS                    : natural := 128;
+  constant PACE_OSD_YPOS                    : natural := 176;
+
+end;
