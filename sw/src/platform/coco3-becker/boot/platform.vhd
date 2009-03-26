4df35ee1dd3ca42c426809494e05f07bd13cd2e2
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2009-03-26 22:17:54 +0000

    initial checkin - boot image for Coco3+
    git-svn-id: https://svn.pacedev.net/repos/pace@489 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/src/platform/coco3-becker/boot/platform.vhd
@@ -0,0 +1,136 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+use work.pace_pkg.all;
+use work.video_controller_pkg.all;
+use work.sprite_pkg.all;
+use work.platform_pkg.all;
+
+entity platform is
+  generic
+  (
+    NUM_INPUT_BYTES   : integer
+  );
+  port
+  (
+    -- clocking and reset
+    clk_i           : in std_logic_vector(0 to 3);
+    reset_i         : in std_logic;
+
+    -- misc I/O
+    buttons_i       : in from_BUTTONS_t;
+    switches_i      : in from_SWITCHES_t;
+    leds_o          : out to_LEDS_t;
+
+    -- controller inputs
+    inputs_i        : in from_MAPPED_INPUTS_t(0 to NUM_INPUT_BYTES-1);
+		
+    -- FLASH/SRAM
+    flash_i         : in from_FLASH_t;
+    flash_o         : out to_FLASH_t;
+    sram_i	    : in from_SRAM_t;
+    sram_o	    : out to_SRAM_t;
+    sdram_i	    : in from_SDRAM_t;
+    sdram_o	    : out to_SDRAM_t;
+
+    -- graphics
+    
+    bitmap_i        : in from_BITMAP_CTL_t;
+    bitmap_o        : out to_BITMAP_CTL_t;
+    
+    tilemap_i       : in from_TILEMAP_CTL_t;
+    tilemap_o       : out to_TILEMAP_CTL_t;
+
+    sprite_reg_o    : out to_SPRITE_REG_t;
+    sprite_i        : in from_SPRITE_CTL_t;
+    sprite_o        : out to_SPRITE_CTL_t;
+	spr0_hit	: in std_logic;
+
+    -- various graphics information
+    graphics_i      : in from_GRAPHICS_t;
+    graphics_o      : out to_GRAPHICS_t;
+    
+    -- OSD
+    osd_i           : in from_OSD_t;
+    osd_o           : out to_OSD_t;
+
+    -- sound
+    snd_i           : in from_SOUND_t;
+    snd_o           : out to_SOUND_t;
+
+    -- SPI (flash)
+    spi_i           : in from_SPI_t;
+    spi_o           : out to_SPI_t;
+
+    -- serial
+    ser_i           : in from_SERIAL_t;
+    ser_o           : out to_SERIAL_t;
+
+    -- general purpose I/O
+    gp_i            : in from_GP_t;
+    gp_o            : out to_GP_t
+  );
+end entity platform;
+
+architecture SYN of platform is
+
+	alias clk_20M					: std_logic is clk_i(0);
+	alias clk_video       : std_logic is clk_i(1);
+	signal clk_2M_en			: std_logic;
+	
+  
+begin
+
+	tilerom_inst : entity work.sprom
+		generic map
+		(
+			init_file		=> "../../../../../src/platform/coco3-becker/roms/coco3gen.hex",
+			numwords_a	=> 2048,
+			width_a     => 16,
+			widthad_a		=> 12
+		)
+		port map
+		(
+			clock			      => clk_video,
+			address		      => tilemap_i.tile_a(11 downto 0),
+			q(15 downto 8)  => tilemap_o.attr_d(7 downto 0),
+			q(7 downto 0)   => tilemap_o.tile_d
+		);
+	
+  -- wren_a *MUST* be GND for CYCLONEII_SAFE_WRITE=VERIFIED_SAFE
+	vram_inst : entity work.dpram
+		generic map
+		(
+			init_file		=> "../../../../../src/platform/trs80/m3/roms/trsvram.hex",
+			numwords_a	=> 1024,
+			widthad_a		=> 10
+		)
+		port map
+		(
+			clock_b			=> clk_20M,
+			address_b		=> (others => '0'),
+			wren_b			=> '0',
+			data_b			=> (others => '0'),
+			q_b					=> open,
+	
+		  clock_a			=> clk_video,
+			address_a		=> tilemap_i.map_a(9 downto 0),
+			wren_a			=> '0',
+			data_a			=> (others => 'X'),
+			q_a					=> tilemap_o.map_d(7 downto 0)
+		);
+    tilemap_o.map_d(tilemap_o.map_d'left downto 8) <= (others => '0');
+    
+  -- unused outputs
+	bitmap_o <= NULL_TO_BITMAP_CTL;
+	sprite_reg_o <= NULL_TO_SPRITE_REG;
+	sprite_o <= NULL_TO_SPRITE_CTL;
+  tilemap_o.attr_d <= std_logic_vector(resize(unsigned(switches_i(7 downto 0)), tilemap_o.attr_d'length));
+	graphics_o <= NULL_TO_GRAPHICS;
+	ser_o <= NULL_TO_SERIAL;
+  spi_o <= NULL_TO_SPI;
+  gp_o <= NULL_TO_GP;
+
+end architecture SYN;
---
+++ sw/src/platform/coco3-becker/boot/tilemapctl.vhd
@@ -0,0 +1,126 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+library work;
+use work.pace_pkg.all;
+use work.platform_pkg.all;
+use work.video_controller_pkg.all;
+use work.project_pkg.all;
+
+--
+--	TRS-80 Tilemap Controller
+--
+--	Tile data is 1 BPP.
+--
+
+-- NOTE: this is currently broken when borders = 0
+-- - eg. 1024x768 x2 
+--   because the controller comes out of hblank (pipelined) when vblank is not asserted
+--   and then vcount is incremented before the 1st line starts displaying
+--
+
+entity tilemapCtl_1 is          
+  generic
+  (
+    DELAY       : integer
+  );          
+  port               
+  (
+    reset				: in std_logic;
+
+    -- video control signals		
+    video_ctl   : in from_VIDEO_CTL_t;
+
+    -- tilemap controller signals
+    ctl_i       : in to_TILEMAP_CTL_t;
+    ctl_o       : out from_TILEMAP_CTL_t;
+
+    graphics_i  : in to_GRAPHICS_t
+  );
+end tilemapCtl_1;
+
+architecture SYN of tilemapCtl_1 is
+
+  alias clk       : std_logic is video_ctl.clk;
+  alias clk_ena   : std_logic is video_ctl.clk_ena;
+  alias stb       : std_logic is video_ctl.stb;
+  alias hblank    : std_logic is video_ctl.hblank;
+  alias vblank    : std_logic is video_ctl.vblank;
+  alias x         : std_logic_vector(video_ctl.x'range) is video_ctl.x;
+  alias y         : std_logic_vector(video_ctl.y'range) is video_ctl.y;
+  
+begin
+
+	-- these are constant for a whole line
+  ctl_o.map_a(ctl_o.map_a'left downto 10) <= (others => '0');
+  ctl_o.map_a(9 downto 6) <= y(6+PACE_VIDEO_V_SCALE downto 3+PACE_VIDEO_V_SCALE);
+  ctl_o.tile_a(ctl_o.tile_a'left downto 12) <= (others => '0');
+  ctl_o.tile_a(3 downto 0) <= y(2+PACE_VIDEO_V_SCALE downto -1+PACE_VIDEO_V_SCALE);
+
+  -- generate attribute RAM address (not used)
+  ctl_o.attr_a <= (others => '0');
+
+  -- generate pixel
+  process (clk, clk_ena, reset)
+
+		constant X_PIPELINE_BITS  : integer := 4;
+		variable x_r		    : std_logic_vector((DELAY-1)*X_PIPELINE_BITS-1 downto 0);
+		variable pel 				: std_logic;
+		
+  begin
+  
+		if reset = '1' then
+			null;
+  	elsif rising_edge(clk) and clk_ena = '1' then
+
+      -- 1st stage of pipeline
+      -- - read tile from tilemap
+      if stb = '1' then
+        ctl_o.map_a(5 downto 1) <= x(8 downto 4);
+        ctl_o.map_a(0) <= x(3);
+      end if;
+
+      -- 2nd stage of pipeline
+      -- - read tile data from tile ROM
+      ctl_o.tile_a(11 downto 4) <= ctl_i.map_d(7 downto 0);
+
+      -- 3rd stage of pipeline
+      -- - assign pixel colour based on tile data
+      -- (each byte contains information for 8 pixels)
+      case x_r(x_r'left-1 downto x_r'left-3) is
+        when "000" =>
+          pel := ctl_i.tile_d(0);
+        when "001" =>
+          pel := ctl_i.tile_d(1);
+        when "010" =>
+          pel := ctl_i.tile_d(2);
+        when "011" =>
+          pel := ctl_i.tile_d(3);
+        when "100" =>
+          pel := ctl_i.tile_d(4);
+        when "101" =>
+          pel := ctl_i.tile_d(5);
+        when "110" =>
+          pel := ctl_i.tile_d(6);
+        when others =>
+          pel := ctl_i.tile_d(7);
+      end case;
+
+      -- green-screen display
+      ctl_o.rgb.r <= (others => '0');
+      ctl_o.rgb.g <= (others => pel);
+      ctl_o.rgb.b <= (others => '0');
+      
+			-- pipelined because of tile data loopkup
+      x_r := x_r(x_r'left-X_PIPELINE_BITS downto 0) & x(X_PIPELINE_BITS-1 downto 0);
+
+      ctl_o.set <= pel;
+
+		end if; -- rising_edge(clk)
+
+  end process;
+
+end SYN;
+
