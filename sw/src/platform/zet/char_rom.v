07db86f0b18f1e4701c8838065c7555f6353e45c
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2009-04-28 05:23:51 +0000

    updated to v0.5.2
    git-svn-id: https://svn.pacedev.net/repos/pace@539 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/src/platform/zet/char_rom.v
+++ sw/src/platform/zet/char_rom.v
@@ -1,14 +1,14 @@
 module char_rom (
-    input  [11:0] addr,
-    output [ 7:0] q
+    input             clk,
+    input      [11:0] addr,
+    output reg [ 7:0] q
   );
 
   // Registers, nets and parameters
   reg [7:0] rom[0:4095];
 
-  // Assignments
-  assign q = rom[addr];
-
   // Behaviour
+  always @(posedge clk) q <= rom[addr];
+
   initial $readmemh("../../../../src/platform/zet/unzip/cores/vga/rtl/char_rom.dat", rom);
 endmodule
