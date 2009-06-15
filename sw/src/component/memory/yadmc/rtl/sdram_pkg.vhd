e43044169e38e165d2a2d2b369e73f5a2099d374
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2009-06-15 05:25:26 +0000

    added package for yadmc module
    git-svn-id: https://svn.pacedev.net/repos/pace@565 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/src/component/memory/yadmc/rtl/sdram_pkg.vhd
@@ -0,0 +1,27 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+
+package sdram_pkg is
+
+  -- Types
+
+	type from_SDRAM_t is record
+		d					: std_logic_vector(31 downto 0);
+		ack       : std_logic;
+	end record;
+	
+	type to_SDRAM_t is record
+    clk       : std_logic;
+    rst       : std_logic;
+		a					: std_logic_vector(31 downto 0);
+		d					: std_logic_vector(31 downto 0);
+		sel			  : std_logic_vector(3 downto 0);
+		cyc			  : std_logic;
+		stb			  : std_logic;
+		we				: std_logic;
+	end record;
+
+end;
