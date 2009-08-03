f55881ddd249dbb00a9e101133df5add9d209805
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2009-08-03 15:27:26 +0000

    fixed adventurevision (DE1)
    git-svn-id: https://svn.pacedev.net/repos/pace@605 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/src/platform/adventurevision/pace.vhd
+++ sw/src/platform/adventurevision/pace.vhd
@@ -192,8 +192,8 @@ architecture SYN of PACE is
   signal gnd8_s           : std_logic_vector( 7 downto 0);
 
 	-- changes for PACE
-  alias ext_clk_i            : std_logic is clk_i(2);
-	alias clk_20M								: std_logic is clk_i(0);
+  alias ext_clk_i            : std_logic is clk_i(0);
+	alias clk_20M								: std_logic is clk_i(1);
   signal clk_cnt_q           : unsigned(1 downto 0);
 	signal clk_en_5m37_q			 : std_logic;
 	
