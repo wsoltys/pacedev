1319a66748897f69aafb41f79bea871f52d1e559
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2009-09-09 14:48:09 +0000

    added ps2 host module - nothing is tested yet
    git-svn-id: https://svn.pacedev.net/repos/pace@617 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/src/component/ps2/ps2host.vhd
@@ -0,0 +1,127 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity ps2_host is
+  generic
+  (
+    CLK_HZ          : natural
+  );
+  port
+  (
+    clk             : in std_logic;
+    reset           : in std_logic;
+
+    -- FIFO interface
+    fifo_data       : in std_logic_vector(7 downto 0);
+    fifo_wrreq      : in std_logic;
+    fifo_full       : out std_logic;
+    fifo_usedw      : out std_logic_vector(7 downto 0);
+        
+    -- PS/2 lines
+    ps2_kclk        : out std_logic;
+    ps2_kdat        : out std_logic
+  );
+end entity ps2_host;
+
+architecture SYN of ps2_host is
+
+  constant CLK_66k667_div : natural := CLK_HZ / 66667;
+  
+  signal clk_66k667_en    : std_logic := '0';
+  signal ps2_data_r       : std_logic_vector(10 downto 0) := (others => '0');
+  signal parity           : std_logic := '0';
+  
+  signal fifo_q           : std_logic_vector(7 downto 0) := (others => '0');
+  signal fifo_rdreq       : std_logic := '0';
+  signal fifo_empty       : std_logic := '0';
+  
+  type state_t is ( IDLE, SEND1, SEND2, SEND3, SEND4 );
+  signal state            : state_t;
+  
+begin
+
+  -- generate 66.667kHz clock enable
+  process (clk, reset)
+    type count_t is integer range 0 to CLK_66k667_div-1;
+    variable count : count_t := 0;
+  begin
+    if reset = '1' then
+      count := 0;
+    elsif rising_edge(clk) then
+      clk_66k667_en <= '0'; -- default
+      if count = count_t'high then
+        clk_66k667_en <= '1';
+        count := 0;
+      else
+        count := count + 1;
+      end if;
+    end if;
+  end process;
+
+  -- generate (odd) parity
+  parity <= not ( fifo_q(7) xor fifo_q(6) xor fifo_q(5) xor fifo_q(4) xor 
+                  fifo_q(3) xor fifo_q(2) xor fifo_q(1) xor fifo_q(0) );
+  
+  process (clk, reset)
+    variable count : integer range 0 to ps2_data_r'left;
+  begin
+    if reset = '1' then
+      state <= IDLE;
+      ps2_kclk <= '0';
+      ps2_kdat <= '0';
+      fifo_rdreq <= '0';
+    elsif rising_edge(clk) and clk_66k667_en = '1' then
+      fifo_rdreq <= '0';  -- default
+      case state is
+        when IDLE =>
+          ps2_kclk <= '0';
+          ps2_kdat <= '0';
+          if fifo_empty = '0' then
+            -- reverse order (since LSB first)
+            ps2_data_r <= '1' & parity & fifo_q & '0';
+            fifo_rdreq <= '1';
+            count := ps2_data_r'left;
+            state <= SEND1;
+          end if;
+        when SEND1 =>
+          ps2_kclk <= '1';
+          state <= SEND2;
+        when SEND2 =>
+          ps2_kclk <= '1';
+          ps2_kdat <= ps2_data_r(0);
+          state <= SEND3;
+        when SEND3 =>
+          ps2_kclk <= '0';
+          state <= SEND4;
+        when SEND4 =>
+          ps2k_clk <= '0';
+          -- shift data register
+          ps2_data_r <= '0' & ps2_data_r(ps2_data_r'left downto 1);
+          if count = 0 then
+            state <= IDLE;
+          else
+            count := count - 1;
+            state <= SEND1;
+          end if;
+        when others =>
+          state <= IDLE;
+       end case;
+    end if;
+  end process;
+
+  fifo_inst : entity work.ps2_fifo
+  	PORT
+  	(
+  		clock		=> clk,
+
+  		data		=> fifo_data,
+  		wrreq		=> fifo_wrreq,
+  		full		=> fifo_full,
+  		usedw		=> fifo_usedw,
+  		
+  		q		    => fifo_q,
+  		rdreq		=> fifo_rdreq,
+  		empty		=> fifo_empty
+  	);
+
+end architecture ps2_host;
---
+++ sw/src/component/ps2/tb/ps2_tb.vhd
@@ -0,0 +1,106 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use work.ext.all;
+
+entity ps2_tb is
+	port (
+		fail:				out  boolean
+	);
+end ps2_tb;
+
+architecture SYN of ps2_tb is
+
+	signal clk_24M576   : std_logic	:= '0';
+	signal clk_32M      : std_logic	:= '0';
+	signal clk_1M_en    : std_logic	:= '0';
+	signal reset			  : std_logic	:= '1';
+  signal reset_n      : std_logic	:= '0';
+  
+  signal ps2_kclk     : std_logic := '0';
+  signal ps2_kdat     : std_logic := '0';
+  
+  signal fifo_data    : std_logic_vector(7 downto 0) := (others => '0');
+  signal fifo_wrreq   : std_logic := '0';
+  signal fifo_full    : std_logic := '0';
+  
+	signal sim_done		  : boolean := false;
+	
+begin
+
+	-- Generate CLK and reset
+	clk_24M576 <= not clk_24M576 after 20345 ps when not sim_done else '0';
+	clk_32M <= not clk_32M after 15625 ps when not sim_done else '0';
+	reset <= '0' after 100 ns;
+  reset_n <= not reset;
+  
+	tb : process
+	begin
+
+    wait until reset = '0';
+    wait until rising_edge(clk_24M576);
+
+    wait until rising_edge(clk_24M576);
+    fifo_data <= X"1C";
+    fifo_wrreq <= '1';
+    wait until falling_edge(clk_24M576);
+    fifo_wrreq <= '0';
+
+		wait for 1000 ms;
+
+		sim_done <= true;
+		wait;
+	end process;
+
+  host_inst : entity work.ps2_host
+    generic map
+    (
+      CLK_HZ          => 24576000
+    );
+    port map
+    (
+      clk             => clk_24M576,
+      reset           => reset,
+  
+      -- FIFO interface
+      fifo_data       => fifo_data,
+      fifo_wrreq      => fifo_wrreq,
+      fifo_full       => fifo_full,
+      fifo_usedw      => open,
+          
+      -- PS/2 lines
+      ps2_kclk        => ps2_kclk,
+      ps2_kdat        => ps2_kdat
+    );
+
+  -- generate clk_1M_ena
+  process (clk_32M, reset)
+    variable count := 0 to 31;
+  begin
+    if reset = '1' then
+      count := 0;
+    elsif rising_edge(clk_32M) then
+      clk_1M_ena <= '0';  -- default
+      if count = 31 then
+        clk_1M_en <= '1';
+        count := 0;
+      else
+        count := count + 1;
+      end if;
+  end process;
+  
+  device_inst : entity work.ps2kbd
+  	port map
+  	(
+  		Rst_n		        => reset_n,
+  		Clk			        => clk_32M,
+  		Tick1us		      => clk_1M_en,
+  		PS2_Clk		      => ps2_kclk,
+  		PS2_Data	      => ps2_kdat,
+  		Press		        => open,
+  		Release		      => open,
+  		Reset		        => open,
+  		ScanCode	      => open
+    );
+
+end SYN;
