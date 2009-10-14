c98cbfd9ae659c5dc19e99217d9898bbb940e8a1
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2009-10-14 22:04:46 +0000

    fleshed out top level
    git-svn-id: https://svn.pacedev.net/repos/pace@670 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/src/target/cb/target_top.vhd
+++ sw/src/target/cb/target_top.vhd
@@ -100,25 +100,189 @@ end target_top;
 
 architecture SYN of target_top is
 
+  alias clk_14M31818    : std_logic is sysclk;
+
   -- sigma-delta audio interface
-  alias audio_left    : std_logic is sda;
-  alias audio_right   : std_logic is scl;
-  
-  -- vga interface
-  alias vga_r0        : std_logic is clk_ext2;
-  alias vga_r1        : std_logic is pbio(4);
-  alias vga_g0        : std_logic is pbio(0);
-  alias vga_g1        : std_logic is pbio(1);
-  alias vga_b0        : std_logic is pbio(2);
-  alias vga_b1        : std_logic is pbio(3);
-  alias vga_hs        : std_logic is pbio(45);
-  alias vga_vs        : std_logic is pbio(46);
-  
-  -- sd card interface
-  alias data1         : std_logic is pbio(47);
-  alias data2         : std_logic is pbio(48);
-  alias data3         : std_logic is pbio(49);
+  alias audio_left      : std_logic is sda;
+  alias audio_right     : std_logic is scl;
+                        
+  -- vga interface      
+  alias vga_r0          : std_logic is clk_ext2;
+  alias vga_r1          : std_logic is pbio(4);
+  alias vga_g0          : std_logic is pbio(0);
+  alias vga_g1          : std_logic is pbio(1);
+  alias vga_b0          : std_logic is pbio(2);
+  alias vga_b1          : std_logic is pbio(3);
+  alias vga_hs          : std_logic is pbio(45);
+  alias vga_vs          : std_logic is pbio(46);
+                        
+  -- sd card interface  
+  alias data1           : std_logic is pbio(47);
+  alias data2           : std_logic is pbio(48);
+  alias data3           : std_logic is pbio(49);
+
+  -- PACE stuff
+
+	signal clk_i			    : std_logic_vector(0 to 3);
+  signal init       	  : std_logic := '1';
+  signal reset_i     	  : std_logic := '1';
+	signal reset_n			  : std_logic := '0';
+
+  signal buttons_i      : from_BUTTONS_t;
+  signal switches_i     : from_SWITCHES_t;
+  signal leds_o         : to_LEDS_t;
+  signal inputs_i       : from_INPUTS_t;
+  signal flash_i        : from_FLASH_t;
+  signal flash_o        : to_FLASH_t;
+	signal sram_i			    : from_SRAM_t;
+	signal sram_o			    : to_SRAM_t;	
+	signal sdram_i        : from_SDRAM_t;
+	signal sdram_o        : to_SDRAM_t;
+	signal video_i        : from_VIDEO_t;
+  signal video_o        : to_VIDEO_t;
+  signal audio_i        : from_AUDIO_t;
+  signal audio_o        : to_AUDIO_t;
+  signal ser_i          : from_SERIAL_t;
+  signal ser_o          : to_SERIAL_t;
+  signal project_i      : from_PROJECT_IO_t;
+  signal project_o      : to_PROJECT_IO_t;
+  signal platform_i     : from_PLATFORM_IO_t;
+  signal platform_o     : to_PLATFORM_IO_t;
+  signal target_i       : from_TARGET_IO_t;
+  signal target_o       : to_TARGET_IO_t;
 
 begin
+
+  BLK_INIT : block
+    signal init : std_logic := '0';
+  begin
+    -- FPGA STARTUP
+  	-- should extend power-on reset if registers init to '0'
+  	process (clk_14M31818)
+	  	variable count : std_logic_vector (11 downto 0) := (others => '0');
+	  begin
+  		if rising_edge(clk_14M31818) then
+  			if count = X"FFF" then
+  				init <= '0';
+  			else
+  				count := count + 1;
+  				init <= '1';
+  			end if;
+  		end if;
+  	end process;
+
+    reset_i <= init;
+  	reset_n <= not reset_i;
+
+	end block BLK_INIT;
+
+  -- no buttons or switches on the CB
+  buttons_i <= (others => '0');
+  switches_i <= (others => '0');
+
+  BLK_INPUTS : block
+  begin
+  end block BLK_INPUTS;
+
+  -- no flash
+  flash_i.d <= (others => 'X');
+
+  BLK_SRAM : block
+
+    GEN_SRAM : if PACE_HAS_SRAM generate
+      sram_a <= sram_o.a(sram_a'range);
+      sram_d <= sram_o.d(sram_d'range) when sram_o.cs = '1' and sram_o.we = '1' else (others => 'Z');
+      sram_ncs <= not sram_o.cs;
+      sram_noe <= not sram_o.oe;
+      sram_nwe <= not sram_o.we;
+      sram_i.d <= std_logic_vector(resize(sram_d),sram_i.d'length);
+    end generate GEN_SRAM;
+
+    GEN_NO_SRAM : if not PACE_HAS_SRAM generate
+      sram_a <= (others => 'Z');
+      sram_d <= (others => 'Z');
+      sram_ncs <= '1';
+      sram_noe <= '1';
+      sram_new <= '1';
+      sram_i.d <= (others => 'X');
+    end generate GEN_NO_SRAM;
+
+  end block BLK_SRAM;
+
+  -- no sdram
+  sdram_o.d <= (others => 'X');
+
+  BLK_VIDEO : block
+  begin
+
+    video_i.clk <= clk_i(1);    -- by convention
+    video_i.clk_ena <= '1';
+    video_i.reset <= reset_i;
+
+    vga_r1 <= video_o.rgb.r(video_o.rgb.r'left);
+    vga_r0 <= video_o.rgb.r(video_o.rgb.r'left-1);
+    vga_g1 <= video_o.rgb.g(video_o.rgb.g'left);
+    vga_g0 <= video_o.rgb.g(video_o.rgb.g'left-1);
+    vga_b1 <= video_o.rgb.b(video_o.rgb.b'left);
+    vga_b0 <= video_o.rgb.b(video_o.rgb.b'left-1);
+    vga_hs <= video_o.hsync;
+    vga_vs <= video_o.vsync;
+
+  end block BLK_VIDEO;
+  
+  BLK_AUDIO : block
+  begin
+  end block BLK_AUDIO;
+
+  -- no serial (atm)
+  ser_i.rxd <= '0';
+
+  pace_inst : entity work.pace                                            
+    port map
+    (
+    	-- clocks and resets
+	  	clk_i							=> clk_i,
+      reset_i          	=> reset_i,
+
+      -- misc inputs and outputs
+      buttons_i         => buttons_i,
+      switches_i        => switches_i,
+      leds_o            => leds_o,
+      
+      -- controller inputs
+      inputs_i          => inputs_i,
+
+     	-- external ROM/RAM
+     	flash_i           => flash_i,
+      flash_o           => flash_o,
+      sram_i        		=> sram_i,
+      sram_o        		=> sram_o,
+     	sdram_i           => sdram_i,
+     	sdram_o           => sdram_o,
+  
+      -- VGA video
+      video_i           => video_i,
+      video_o           => video_o,
+      
+      -- sound
+      audio_i           => audio_i,
+      audio_o           => audio_o,
+
+      -- SPI (flash)
+      spi_i.din         => '0',
+      spi_o             => open,
+  
+      -- serial
+      ser_i             => ser_i,
+      ser_o             => ser_o,
+      
+      -- custom i/o
+      project_i         => project_i,
+      project_o         => project_o,
+      platform_i        => platform_i,
+      platform_o        => platform_o,
+      target_i          => target_i,
+      target_o          => target_o
+    );
 
 end architecture SYN;
