56be21b45cf4b1c690bd10deb3e7dd874fa9350f
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2009-10-19 03:52:12 +0000

    space invaders up-to-date for RC10
    git-svn-id: https://svn.pacedev.net/repos/pace@675 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/src/target/rc10/target_top.vhd
+++ sw/src/target/rc10/target_top.vhd
@@ -6,10 +6,11 @@ use ieee.numeric_std.all;
 
 library work;
 use work.pace_pkg.all;
+use work.sdram_pkg.all;
 use work.video_controller_pkg.all;
-use work.project_pkg.all;
-use work.platform_pkg.all;
 use work.target_pkg.all;
+use work.platform_pkg.all;
+use work.project_pkg.all;
 
 entity target_top is
 port
@@ -131,25 +132,33 @@ end target_top;
 
 architecture SYN of target_top is
 
-	signal clk_i			  : std_logic_vector(0 to 3);
-  signal init       	: std_logic := '1';
-  signal reset_i     	: std_logic := '1';
-	signal reset_n			: std_logic := '0';
+	signal clk_i			    : std_logic_vector(0 to 3);
+  signal init       	  : std_logic := '1';
+  signal reset_i     	  : std_logic := '1';
+	signal reset_n			  : std_logic := '0';
 
-  signal buttons_i    : from_BUTTONS_t;
-  signal switches_i   : from_SWITCHES_t;
-  signal leds_o       : to_LEDS_t;
-  signal inputs_i     : from_INPUTS_t;
-  signal flash_i      : from_FLASH_t;
-  signal flash_o      : to_FLASH_t;
-	signal sram_i			  : from_SRAM_t;
-	signal sram_o			  : to_SRAM_t;	
-	signal video_i      : from_VIDEO_t;
-  signal video_o      : to_VIDEO_t;
-  signal audio_i      : from_AUDIO_t;
-  signal audio_o      : to_AUDIO_t;
-  signal ser_i        : from_SERIAL_t;
-  signal ser_o        : to_SERIAL_t;
+  signal buttons_i      : from_BUTTONS_t;
+  signal switches_i     : from_SWITCHES_t;
+  signal leds_o         : to_LEDS_t;
+  signal inputs_i       : from_INPUTS_t;
+  signal flash_i        : from_FLASH_t;
+  signal flash_o        : to_FLASH_t;
+	signal sram_i			    : from_SRAM_t;
+	signal sram_o			    : to_SRAM_t;	
+	signal sdram_i        : from_SDRAM_t;
+	signal sdram_o        : to_SDRAM_t;
+	signal video_i        : from_VIDEO_t;
+  signal video_o        : to_VIDEO_t;
+  signal audio_i        : from_AUDIO_t;
+  signal audio_o        : to_AUDIO_t;
+  signal ser_i          : from_SERIAL_t;
+  signal ser_o          : to_SERIAL_t;
+  signal project_i      : from_PROJECT_IO_t;
+  signal project_o      : to_PROJECT_IO_t;
+  signal platform_i     : from_PLATFORM_IO_t;
+  signal platform_o     : to_PLATFORM_IO_t;
+  signal target_i       : from_TARGET_IO_t;
+  signal target_o       : to_TARGET_IO_t;
   
 begin
 
@@ -367,7 +376,9 @@ begin
       flash_o           => flash_o,
       sram_i        		=> sram_i,
       sram_o        		=> sram_o,
-  
+     	sdram_i           => sdram_i,
+     	sdram_o           => sdram_o,
+
       -- VGA video
       video_i           => video_i,
       video_o           => video_o,
@@ -384,9 +395,13 @@ begin
       ser_i             => ser_i,
       ser_o             => ser_o,
       
-      -- general purpose
-      gp_i              => (others => '0'),
-      gp_o              => open
+      -- custom i/o
+      project_i         => project_i,
+      project_o         => project_o,
+      platform_i        => platform_i,
+      platform_o        => platform_o,
+      target_i          => target_i,
+      target_o          => target_o
     );
   
 	-- flash the led so we know it's alive
