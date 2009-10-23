5ea00340f7d3ad9f0bc66562f2eadf42fd4ce5e3
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2009-10-23 04:58:16 +0000

    added some more cb midway projects
    git-svn-id: https://svn.pacedev.net/repos/pace@691 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/synth/platform/midway8080/galxwars/cb/project_pkg.vhd
@@ -0,0 +1,54 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+library work;
+use work.pace_pkg.all;
+use work.video_controller_pkg.all;
+use work.target_pkg.all;
+
+package project_pkg is
+
+	--  
+	-- PACE constants which *MUST* be defined
+	--
+
+	constant PACE_HAS_PLL								      : boolean := true;
+  --constant PACE_HAS_SRAM                    : boolean := false;
+	
+  -- Reference clock is 48MHz
+  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_800x600_60Hz;
+  constant PACE_CLK0_DIVIDE_BY              : natural := 7;   -- 14.31818*10/7 = 20.45MHz
+  constant PACE_CLK0_MULTIPLY_BY            : natural := 10;  -- (used by SI)
+  constant PACE_CLK1_DIVIDE_BY              : natural := 7;
+  constant PACE_CLK1_MULTIPLY_BY            : natural := 20;  -- 14.31818*20/7 = 40.9MHz
+  constant PACE_VIDEO_H_SCALE       	      : integer := 2;
+  constant PACE_VIDEO_V_SCALE       	      : integer := 2;
+
+  constant PACE_VIDEO_BORDER_RGB            : RGB_t := RGB_BLUE;
+  --constant PACE_VIDEO_BORDER_RGB            : RGB_t := RGB_BLACK;
+  
+  constant PACE_HAS_OSD                     : boolean := false;
+  constant PACE_OSD_XPOS                    : natural := 0;
+  constant PACE_OSD_YPOS                    : natural := 0;
+
+  --
+	-- Space Invaders-specific constants
+	--
+	
+  constant INVADERS_ROM_IN_FLASH            : boolean := PACE_HAS_FLASH;
+	constant INVADERS_USE_INTERNAL_WRAM				: boolean := false;
+
+  -- derived - do not edit
+  
+  constant PACE_HAS_SRAM                    : boolean := not INVADERS_USE_INTERNAL_WRAM;
+	
+  type from_PROJECT_IO_t is record
+    not_used  : std_logic;
+  end record;
+
+  type to_PROJECT_IO_t is record
+    not_used  : std_logic;
+  end record;
+
+end;
