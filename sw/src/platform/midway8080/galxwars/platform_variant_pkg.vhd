0b6597053cbe6b1330c201caf7f90a008fa1a562
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2009-10-23 04:15:46 +0000

    rc10 midway projects working
    git-svn-id: https://svn.pacedev.net/repos/pace@690 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/src/platform/midway8080/galxwars/platform_variant_pkg.vhd
+++ sw/src/platform/midway8080/galxwars/platform_variant_pkg.vhd
@@ -8,8 +8,8 @@ package platform_variant_pkg is
 	-- Platform-variant-specific constants (optional)
 	--
 
-	constant ROM_1_NAME		: string := "../../../../../src/platform/midway8080/galxwars/roms/galxwars0.hex";
-	constant ROM_2_NAME		: string := "../../../../../src/platform/midway8080/galxwars/roms/galxwars1.hex";
+	constant ROM_0_NAME		: string := "../../../../../src/platform/midway8080/galxwars/roms/galxwars0.hex";
+	constant ROM_1_NAME		: string := "../../../../../src/platform/midway8080/galxwars/roms/galxwars1.hex";
 	constant VRAM_NAME		: string := "../../../../../src/platform/midway8080/invaders/roms/sivram.hex";
 	
 end;
--- sw/src/platform/midway8080/invadpt2/platform_variant_pkg.vhd
+++ sw/src/platform/midway8080/invadpt2/platform_variant_pkg.vhd
@@ -8,8 +8,8 @@ package platform_variant_pkg is
 	-- Platform-variant-specific constants (optional)
 	--
 
-	constant ROM_1_NAME		: string := "../../../../../src/platform/midway8080/invadpt2/roms/invadpt20.hex";
-	constant ROM_2_NAME		: string := "../../../../../src/platform/midway8080/invadpt2/roms/invadpt21.hex";
+	constant ROM_0_NAME		: string := "../../../../../src/platform/midway8080/invadpt2/roms/invadpt20.hex";
+	constant ROM_1_NAME		: string := "../../../../../src/platform/midway8080/invadpt2/roms/invadpt21.hex";
 	constant VRAM_NAME		: string := "../../../../../src/platform/midway8080/invaders/roms/sivram.hex";
 	
 end;
