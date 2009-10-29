93861fc58eab1cd381312e991b84576ca92a2608
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2009-10-29 07:16:11 +0000

    cb pacman now builds - hopefully runs too!?!
    git-svn-id: https://svn.pacedev.net/repos/pace@719 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/synth/platform/pacman/cb/project_pkg.vhd
+++ sw/synth/platform/pacman/cb/project_pkg.vhd
@@ -15,8 +15,11 @@ package project_pkg is
 
 	constant PACE_HAS_PLL								      : boolean := true;
   --constant PACE_HAS_SRAM                    : boolean := false;
-	
-  -- Reference clock is 48MHz
+  constant PACE_HAS_AUDIO                   : boolean := true;
+  
+	constant PACE_JAMMA	                      : PACEJamma_t := PACE_JAMMA_PS2;
+
+  -- Reference clock is 14.31818MHz
   constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_800x600_60Hz;
   constant PACE_CLK0_DIVIDE_BY              : natural := 7;   -- 14.31818*10/7 = 20.45MHz
   constant PACE_CLK0_MULTIPLY_BY            : natural := 10;  -- (used by Pacman)
@@ -25,8 +28,8 @@ package project_pkg is
   constant PACE_VIDEO_H_SCALE       	      : integer := 2;
   constant PACE_VIDEO_V_SCALE       	      : integer := 2;
 
-  constant PACE_VIDEO_BORDER_RGB            : RGB_t := RGB_BLUE;
-  --constant PACE_VIDEO_BORDER_RGB            : RGB_t := RGB_BLACK;
+  --constant PACE_VIDEO_BORDER_RGB            : RGB_t := RGB_BLUE;
+  constant PACE_VIDEO_BORDER_RGB            : RGB_t := RGB_BLACK;
   
   constant PACE_HAS_OSD                     : boolean := false;
   constant PACE_OSD_XPOS                    : natural := 0;
@@ -35,13 +38,15 @@ package project_pkg is
   --
 	-- Pacman-specific constants
 	--
-	
+
+  constant PACMAN_ROM_IN_SRAM               : boolean := true;
 	constant PACMAN_USE_INTERNAL_WRAM				  : boolean := false;
 	constant PACMAN_USE_VIDEO_VBLANK          : boolean := true;
 
   -- derived - do not edit
   
-  constant PACE_HAS_SRAM                    : boolean := not PACMAN_USE_INTERNAL_WRAM;
+  constant PACE_HAS_SRAM                    : boolean := PACMAN_ROM_IN_SRAM or
+                                                          not PACMAN_USE_INTERNAL_WRAM;
 	
   type from_PROJECT_IO_t is record
     not_used  : std_logic;
