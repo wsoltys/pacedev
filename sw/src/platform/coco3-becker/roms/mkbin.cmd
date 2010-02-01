bcd9efba967eaa07d0a1391fa190d47dd91e5979
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2010-02-01 11:16:28 +0000

    for external rom builds
    git-svn-id: https://svn.pacedev.net/repos/pace@739 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/src/platform/coco3-becker/roms/mkbin.cmd
@@ -0,0 +1,47 @@
+init2bin CART_C0.V
+rename init2bin.out CART_C0.bin
+init2bin CC3_80.v
+rename init2bin.out CC3_80.bin
+init2bin CC3_88.v
+rename init2bin.out CC3_88.bin
+init2bin CC3_90.v
+rename init2bin.out CC3_90.bin
+init2bin CC3_98.v
+rename init2bin.out CC3_98.bin
+init2bin CC3_A0.v
+rename init2bin.out CC3_A0.bin
+init2bin CC3_A8.v
+rename init2bin.out CC3_A8.bin
+init2bin CC3_B0.v
+rename init2bin.out CC3_B0.bin
+init2bin CC3_B8.v
+rename init2bin.out CC3_B8.bin
+init2bin CC3_C0.v
+rename init2bin.out CC3_C0.bin
+init2bin CC3_C8.v
+rename init2bin.out CC3_C8.bin
+init2bin CC3_D0.v
+rename init2bin.out CC3_D0.bin
+init2bin CC3_D8.v
+rename init2bin.out CC3_D8.bin
+init2bin CC3_E0.v
+rename init2bin.out CC3_E0.bin
+init2bin CC3_E8.v
+rename init2bin.out CC3_E8.bin
+init2bin CC3_F0.v
+rename init2bin.out CC3_F0.bin
+init2bin CC3_F8.v
+rename init2bin.out CC3_F8.bin
+init2bin DSK_C0.V
+rename init2bin.out DSK_C0.bin
+init2bin DSK_C8.V
+rename init2bin.out DSK_C8.bin
+init2bin DSK_D0.V
+rename init2bin.out DSK_D0.bin
+init2bin DSK_D8.V
+rename init2bin.out DSK_D8.bin
+init2bin RS232_C0.V
+rename init2bin.out RS232_C0.bin
+init2bin RS232_C8.V
+rename init2bin.out RS232_C8.bin
+copy /b CART_C0.bin+CC3_80.bin+CC3_88.bin+CC3_90.bin+CC3_98.bin+CC3_A0.bin+CC3_A8.bin+CC3_B0.bin+CC3_B8.bin+CC3_C0.bin+CC3_C8.bin+CC3_D0.bin+CC3_D8.bin+CC3_E0.bin+CC3_E8.bin+CC3_F0.bin+CC3_F8.bin+DSK_C0.bin+DSK_C8.bin+DSK_D0.bin+DSK_D8.bin+RS232_C0.bin+RS232_C8.bin coco3roms.bin
