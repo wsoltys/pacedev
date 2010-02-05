5962c7fa6fde6c8941ec17760023d928de194da9
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2010-02-05 00:21:50 +0000

    DE2 version up-to-date runs out of internal ram doesn't run from flash
    git-svn-id: https://svn.pacedev.net/repos/pace@748 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/synth/platform/coco3-becker/de2/project_pkg.vhd
+++ sw/synth/platform/coco3-becker/de2/project_pkg.vhd
@@ -13,22 +13,38 @@ package project_pkg is
 	
 	constant PACE_HAS_PLL										  : boolean := true;
   constant PACE_HAS_SRAM                    : boolean := true;
-  constant PACE_HAS_FLASH                   : boolean := false;
+  --constant PACE_HAS_FLASH                   : boolean := false;
   constant PACE_HAS_SDRAM                   : boolean := false;
   constant PACE_HAS_SERIAL                  : boolean := true;
 	
+	constant PACE_JAMMA	                      : PACEJamma_t := PACE_JAMMA_NONE;
+  
   -- Reference clock is 24MHz
   constant PACE_CLK0_DIVIDE_BY        		  : natural := 1;
   constant PACE_CLK0_MULTIPLY_BY      		  : natural := 1;  	  -- 50MHz
   constant PACE_CLK1_DIVIDE_BY        		  : natural := 1;
   constant PACE_CLK1_MULTIPLY_BY      		  : natural := 2;  	  -- 50MHz
 
-	constant PACE_JAMMA	                      : PACEJamma_t := PACE_JAMMA_NONE;
-  
+  constant PACE_HAS_OSD                     : boolean := false;
+  constant PACE_OSD_XPOS                    : natural := 128;
+  constant PACE_OSD_YPOS                    : natural := 176;
+
 	-- DE2 constants which *MUST* be defined
 	
 	constant DE2_LCD_LINE2							      : string := " COCO3 (BECKER) ";
 
 	-- Coco3-specific constants
+
+  constant COCO3_ROMS_IN_FLASH              : boolean := true;
+  -- derived - do not edit
+	constant PACE_HAS_FLASH                   : boolean := COCO3_ROMS_IN_FLASH;
+
+  type from_PROJECT_IO_t is record
+    not_used  : std_logic;
+  end record;
+
+  type to_PROJECT_IO_t is record
+    not_used  : std_logic;
+  end record;
 
 end;
