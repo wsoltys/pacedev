ceafd3e92c75976690e75d4440b9f25b3de8d5b9
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2010-08-18 04:37:27 +0000

    updated to use latest versions (fixed) of rom files
    git-svn-id: https://svn.pacedev.net/repos/pace@792 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/synth/platform/coco1/de1/project_pkg.vhd
+++ sw/synth/platform/coco1/de1/project_pkg.vhd
@@ -39,9 +39,11 @@ package project_pkg is
 	-- Coco1-specific constants
 
   constant COCO1_USE_REAL_6809              : boolean := true;
-  constant COCO1_BASIC_ROM                  : string := "bas10.hex";
-  --constant COCO1_BASIC_ROM                  : string := "bas11.hex";
-  constant COCO1_EXTENDED_BASIC_ROM         : string := "extbas10.hex";
+  --constant COCO1_BASIC_ROM                  : string := "bas10.hex";
+  constant COCO1_BASIC_ROM                  : string := "bas11.hex";
+  --constant COCO1_BASIC_ROM                  : string := "bas12.hex";
+  --constant COCO1_EXTENDED_BASIC_ROM         : string := "extbas10.hex";
+  constant COCO1_EXTENDED_BASIC_ROM         : string := "extbas11.hex";
   constant COCO1_EXTENDED_COLOR_BASIC       : boolean := false;
   constant COCO1_JUMPER_32K_RAM             : std_logic := '1';
   
