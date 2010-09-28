ca9028261576bb802cef6bfb9039615483886f07
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2010-09-28 22:14:36 +0000

    added dkong for the VEB
    git-svn-id: https://svn.pacedev.net/repos/pace@866 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/synth/platform/dkong/s5a_r2/project_pkg-ep3sl.vhd
@@ -0,0 +1,59 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+library work;
+use work.pace_pkg.all;
+use work.target_pkg.all;
+use work.video_controller_pkg.all;
+
+package project_pkg is
+
+	--  
+	-- PACE constants which *MUST* be defined
+	--
+	
+  -- Reference clock is 24MHz
+	constant PACE_HAS_PLL								      : boolean := true;
+  constant PACE_HAS_SDRAM                   : boolean := false;
+  constant PACE_HAS_SERIAL                  : boolean := false;
+  
+	constant PACE_JAMMA	                      : PACEJamma_t := PACE_JAMMA_NONE;
+
+  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_NONE;
+  constant PACE_CLK0_DIVIDE_BY              : natural := 5;
+  constant PACE_CLK0_MULTIPLY_BY            : natural := 6;     -- 24*5/6 = 20MHz
+  constant PACE_CLK1_DIVIDE_BY              : natural := 1;
+  constant PACE_CLK1_MULTIPLY_BY            : natural := 1;  	  -- 24*1/1 = 24MHz
+
+  -- S5A-specific constants
+  
+  constant S5AR2_DE_GEN                     : std_logic := '0';
+  constant S5AR2_VS_POL                     : std_logic := '0';
+  constant S5AR2_HS_POL                     : std_logic := '0';
+  constant S5AR2_DE_DLY                     : std_logic_vector(11 downto 0) := X"000";
+  constant S5AR2_DE_TOP                     : std_logic_vector(7 downto 0) := X"00";
+  constant S5AR2_DE_CNT                     : std_logic_vector(11 downto 0) := X"000";
+  constant S5AR2_DE_LIN                     : std_logic_vector(11 downto 0) := X"000";
+
+	-- Donkey Kong-specific constants
+  constant DKONG_ROM_IN_FLASH				        : boolean := false;
+  constant DKONG_ROM_IN_SRAM                : boolean := false;
+
+  constant S5AR2_EMULATE_SRAM               : boolean := DKONG_ROM_IN_SRAM;
+  constant S5AR2_EMULATED_SRAM_WIDTH_AD     : natural := 16;
+  constant S5AR2_EMULATED_SRAM_WIDTH        : natural := 8;
+
+  -- derived - do not edit
+  constant PACE_HAS_FLASH                   : boolean := DKONG_ROM_IN_FLASH;
+  constant PACE_HAS_SRAM                    : boolean := DKONG_ROM_IN_SRAM;
+	
+  type from_PROJECT_IO_t is record
+    not_used  : std_logic;
+  end record;
+
+  type to_PROJECT_IO_t is record
+    not_used  : std_logic;
+  end record;
+
+end;
