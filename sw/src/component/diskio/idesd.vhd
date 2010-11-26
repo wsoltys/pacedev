5caa770b7deabd4fa20b0b76b83a2862d9f58558
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2010-11-26 01:50:15 +0000

    gets past execute_device_diagnostics & identify_device - is reading a sector now... woo hoo!
    git-svn-id: https://svn.pacedev.net/repos/pace@909 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/src/component/diskio/idesd.vhd
+++ sw/src/component/diskio/idesd.vhd
@@ -2,7 +2,7 @@ library ieee;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 -- the OCIDE controller uses UNSIGNED from here
-use ieee.std_logic_arith.unsigned;
+--use ieee.std_logic_arith.unsigned;
 
 entity ide_sd is
   port
@@ -101,6 +101,9 @@ architecture SYN of ide_sd is
   signal rd_go        : std_logic := '0';
   signal rd_done      : std_logic := '0';
   
+  signal rom_a        : std_logic_vector(7 downto 0) := (others => '0');
+  signal rom_d        : std_logic_vector(15 downto 0) := (others => '0');
+  
 begin
 
   process (clk, rst)
@@ -164,8 +167,9 @@ begin
     end if;
   end process;
 
-  -- wire-OR signals from CMD, READ SM
-  sts_r_o <= cmd_sts or read_sts;
+  -- BUSY asserted during command unless DRQ asserted
+  sts_r_o(BSY) <= cmd_sts(BSY) and not read_sts(DRQ);
+  sts_r_o(6 downto 0) <= cmd_sts(6 downto 0) or read_sts(6 downto 0);
   
   BLK_MAIN_SM : block
     type state_t is ( S_IDLE, S_DIAGNOSTIC, S_IDENTIFY, S_READ_1, S_WAIT_READ );
@@ -181,6 +185,7 @@ begin
           rd_go <= '0';   -- default
           if cmd_go = '1' then
             cmd_sts(BSY) <= '1';      -- default
+            cmd_sts(DRDY) <= '0';     -- default
             case cmd_r_i is
               when EXEC_DEVICE_DIAGNOSTIC =>
                 state <= S_DIAGNOSTIC;
@@ -190,7 +195,8 @@ begin
               when READ_SECTORS_WO_RETRY =>
                 state <= S_READ_1;
               when others =>
-                cmd_sts(BSY) <= '0';  -- default
+                cmd_sts(BSY) <= '0';
+                cmd_sts(DRDY) <= '0';
             end case;
           else
             case state is
@@ -202,17 +208,18 @@ begin
                 err_r_o <= X"01"; 
                 state <= S_IDLE;
               when S_IDENTIFY =>
+                -- set up some read operation here
+                rd_go <= '1';
+                state <= S_WAIT_READ;
               when S_READ_1 =>
                 -- set up some read operation here
                 rd_go <= '1';
                 state <= S_WAIT_READ;
               when S_WAIT_READ =>
                 if rd_done = '1' then
-                  cmd_sts(BSY) <= '0';
                   state <= S_IDLE;
                 end if;
               when others =>
-                cmd_sts(BSY) <= '0';
                 state <= S_IDLE;
             end case;
           end if; -- cmd_go='1'
@@ -230,28 +237,34 @@ begin
       variable count : count_t := 0;
     begin
       if rst = '1' then
+        read_sts <= (others => '0');
+        state <= S_IDLE;
       elsif rising_edge(clk) then
         if clk_ena = '1' then
+          rd_done <= '0';   -- default
           case state is
             when S_IDLE =>
               read_sts(DRQ) <= '0';   -- default
               if rd_go = '1' then
                 count := 0;
+                rom_a <= (others => '0');
                 state <= S_LATCH_DATA;
               end if;
             when S_LATCH_DATA =>
               -- latch data in register & set data ready
+              data_r_o <= rom_d;
               read_sts(DRQ) <= '1';
-              data_r_o <= std_logic_vector(to_unsigned(count, 16));
               state <= S_WAIT_IORD;
             when S_WAIT_IORD =>
               -- wait for an IORD to increment data
               if data_rd_go = '1' then
                 read_sts(DRQ) <= '0';
                 if count = count_t'high then
+                  rd_done <= '1';
                   state <= S_IDLE;
                 else
                   count := count + 1;
+                  rom_a <= std_logic_vector(unsigned(rom_a) + 1);
                   state <= S_LATCH_DATA;
                 end if;
               end if;
@@ -263,4 +276,19 @@ begin
     end process;
   end block BLK_READ_SM;
   
+  -- COLOR BASIC ROM
+  iderom_inst : entity work.sprom
+		generic map
+		(
+			init_file		=> "../../../../src/platform/coco1/roms/identifydevice.hex",
+			widthad_a   => 8,
+			width_a     => 16
+		)
+  	port map
+  	(
+  		clock		    => clk,
+  		address		  => rom_a(7 downto 0),
+  		q			      => rom_d
+  	);
+
 end architecture SYN;
