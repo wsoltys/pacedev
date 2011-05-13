5513384044280854d4ccacbb7ff3dcf717e9b5b8
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2011-05-13 08:15:48 +0000

    more WIP found 1up/2up strings in sub cpu attempting to find some more strings...
    git-svn-id: https://svn.pacedev.net/repos/pace@980 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/dev/platform/galaga/xevious/ascii.cpp
+++ sw/dev/platform/galaga/xevious/ascii.cpp
@@ -1,16 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 static unsigned char rom[16*1024];
+
+int find_str(char *str, int rom_len)
+{
+  int j;
+  
+  for (int i=0; i<rom_len-strlen(str); i++)
+  {
+    for (j=1; j<strlen(str); j++)
+      if ((rom[i+j]-rom[i]) != (str[j]-str[i]))
+        break;
+    if (j == strlen(str))
+      printf ("Candidate @offset=$%X\n", i);
+  }
+  
+  return (0);
+}
 
 int main (int argc, char *argv[])
 {
   // MAIN
-  
+
+  printf ("main.bin\n");
+    
   FILE *fp = fopen ("main.bin", "rb");
   fread (rom, 16*1024, 1, fp);
   fclose (fp);
 
+  find_str ("WA", 16*124);
+  
   for (int i=0; i<16*1024; i++)
     if (rom[i] < 10)
       rom[i] += '0';
@@ -22,10 +43,14 @@ int main (int argc, char *argv[])
   fclose (fp);
 
   // SUB
+
+  printf ("sub.bin\n");
   
   fp = fopen ("sub.bin", "rb");
   fread (rom, 8*1024, 1, fp);
   fclose (fp);
+
+  find_str ("WARRIORS", 8*124);
 
   for (int i=0; i<8*1024; i++)
     if (rom[i] < 10)
@@ -39,9 +64,13 @@ int main (int argc, char *argv[])
 
   // SUB2
   
+  printf ("sub2.bin\n");
+  
   fp = fopen ("sub2.bin", "rb");
   fread (rom, 4*1024, 1, fp);
   fclose (fp);
+
+  find_str ("WARRIORS", 4*124);
 
   for (int i=0; i<4*1024; i++)
     if (rom[i] < 10)
