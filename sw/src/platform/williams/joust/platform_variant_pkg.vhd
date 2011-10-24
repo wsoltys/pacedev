728f97bb494eccfd711822f4528d444a46103a94
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2011-10-24 14:27:03 +0000

    joust runs but has problems
    git-svn-id: https://svn.pacedev.net/repos/pace@1082 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/src/platform/williams/joust/platform_variant_pkg.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+library work;
+use work.platform_pkg.WILLIAMS_SOURCE_ROOT_DIR;
+
+package platform_variant_pkg is
+
+	--
+	-- Platform-variant-specific constants (optional)
+	--
+
+  constant PLATFORM_VARIANT         : string := "joust";
+  
+  constant VARIANT_SOURCE_ROOT_DIR  : string := WILLIAMS_SOURCE_ROOT_DIR & 
+                                                PLATFORM_VARIANT & "/";
+  constant VARIANT_ROM_DIR          : string := VARIANT_SOURCE_ROOT_DIR &
+                                                "roms/";
+                                                
+end;
