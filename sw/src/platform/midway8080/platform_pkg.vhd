ec648ed63dab8d72accd211440ecf5930c2d5261
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2011-11-24 01:24:49 +0000

    added crop values
    git-svn-id: https://svn.pacedev.net/repos/pace@1147 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/src/platform/midway8080/platform_pkg.vhd
+++ sw/src/platform/midway8080/platform_pkg.vhd
@@ -17,6 +17,8 @@ package platform_pkg is
 	constant PACE_VIDEO_NUM_SPRITES 	    : natural := 0;
 	constant PACE_VIDEO_H_SIZE				    : integer := 224;
 	constant PACE_VIDEO_V_SIZE				    : integer := 256; -- why not 240?
+	constant PACE_VIDEO_L_CROP				    : integer := 0;
+	constant PACE_VIDEO_R_CROP				    : integer := 0;
   constant PACE_VIDEO_PIPELINE_DELAY    : integer := 3;
 	
   constant PACE_INPUTS_NUM_BYTES        : integer := 3;
