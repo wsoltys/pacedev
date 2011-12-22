9cb885b97de27de4116d3b720bf4a8e8fbba29b0
chris <chris@693b85d5-0402-4c10-a79c-edb74d372fca>
2011-12-22 07:23:31 +0000

    "Fixed" IM 2 mcode.
    git-svn-id: https://svn.pacedev.net/repos/pace@1169 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/src/component/cpu/t80/T80_MCode.vhd
+++ sw/src/component/cpu/t80/T80_MCode.vhd
@@ -807,7 +807,8 @@ begin
 				end case;
 			elsif IntCycle = '1' then
 				-- INT (IM 2)
-				MCycles <= "101";
+				--MCycles <= "101";
+				MCycles <= "011";
 				case to_integer(unsigned(MCycle)) is
 				when 1 =>
 					LDZ <= '1';
@@ -824,11 +825,11 @@ begin
 				when 3 =>
 					TStates <= "100";
 					Write <= '1';
-				when 4 =>
-					Inc_PC <= '1';
-					LDZ <= '1';
-				when 5 =>
-					Jump <= '1';
+				--when 4 =>
+				--	Inc_PC <= '1';
+				--	LDZ <= '1';
+				--when 5 =>
+				--	Jump <= '1';
 				when others => null;
 				end case;
 			else
