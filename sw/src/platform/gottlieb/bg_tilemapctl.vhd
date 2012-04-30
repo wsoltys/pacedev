5b825a97f9ea04639bc433705f48d14c26112507
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2012-04-30 13:18:20 +0000

    can read text - sideways
    git-svn-id: https://svn.pacedev.net/repos/pace@1217 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/src/platform/gottlieb/bg_tilemapctl.vhd
+++ sw/src/platform/gottlieb/bg_tilemapctl.vhd
@@ -21,20 +21,18 @@ architecture TILEMAP_1 of tilemapCtl is
   alias stb       : std_logic is video_ctl.stb;
   alias hblank    : std_logic is video_ctl.hblank;
   alias vblank    : std_logic is video_ctl.vblank;
-  alias x         : std_logic_vector(video_ctl.x'range) is video_ctl.x;
-  alias y         : std_logic_vector(video_ctl.y'range) is video_ctl.y;
+  alias tile_x    : std_logic_vector(video_ctl.x'range) is video_ctl.x;
+  alias tile_y    : std_logic_vector(video_ctl.y'range) is video_ctl.y;
+  alias map_x     : std_logic_vector(video_ctl.x'range) is video_ctl.x;
+  alias map_y     : std_logic_vector(video_ctl.y'range) is video_ctl.y;
   
 begin
 
 	-- these are constant for a whole line
   ctl_o.map_a(ctl_o.map_a'left downto 10) <= (others => '0');
-  ctl_o.map_a(4 downto 0) <= y(7 downto 3);
-  ctl_o.tile_a(ctl_o.tile_a'left downto 12) <= (others => '0');
+  ctl_o.map_a(4 downto 0) <= tile_y(7 downto 3);
+  ctl_o.tile_a(ctl_o.tile_a'left downto 13) <= (others => '0');
 
-  -- generate attribute RAM address (same, different memory bank)
-  ctl_o.attr_a(ctl_o.map_a'left downto 10) <= (others => '0');
-  ctl_o.attr_a(4 downto 0) <= y(7 downto 3);
-  
   -- generate pixel
   process (clk, clk_ena)
 
@@ -43,45 +41,35 @@ begin
 		variable pel        : std_logic_vector(3 downto 0);
 		--variable pal_entry  : pal_entry_typ;
 
-		variable x_adj		  : unsigned(x'range);
-    variable tile_d_r   : std_logic_vector(15 downto 0);
+    variable tile_d_r   : std_logic_vector(7 downto 0);
 		variable attr_d_r	  : std_logic_vector(7 downto 0);
 
   begin
 
   	if rising_edge(clk) and clk_ena = '1' then
 
-			-- video is clipped left and right (only 224 wide)
-			x_adj := unsigned(x); -- + (256-PACE_VIDEO_H_SIZE)/2;
-				
       -- 1st stage of pipeline
       -- - read tile from tilemap
       -- - read attribute data
       if stb = '1' then
-        ctl_o.map_a(9 downto 5) <= std_logic_vector(x_adj(7 downto 3));
-        ctl_o.attr_a(9 downto 5) <= not std_logic_vector(x_adj(7 downto 3));
+        ctl_o.map_a(9 downto 5) <= tile_x(7 downto 3);
+        ctl_o.attr_a(9 downto 5) <= not tile_x(7 downto 3);
       end if;
       
       -- 2nd stage of pipeline
       -- - read tile data from tile ROM
-      if stb = '1' then
-        if x_adj(2 downto 0) = "010" then
-          attr_d_r := ctl_i.attr_d(7 downto 0);
-        end if;
-      end if;
-      ctl_o.tile_a(11 downto 4) <= ctl_i.map_d(7 downto 0); -- each tile is 16 bytes/rom
-      ctl_o.tile_a(3 downto 1) <= y(2 downto 0);
-      ctl_o.tile_a(0) <= x_adj(2);
+      ctl_o.tile_a(12 downto 5) <= ctl_i.map_d(7 downto 0); -- each tile is 32 bytes
+      ctl_o.tile_a(4 downto 2) <= tile_y(2 downto 0);
+      ctl_o.tile_a(1 downto 0) <= std_logic_vector(tile_x(2 downto 1));
       
       if stb = '1' then
-        if x_adj(1 downto 0) = "01" then
-          tile_d_r := ctl_i.tile_d(15 downto 0);
+        if tile_x(0) = '0' then
+          tile_d_r := ctl_i.tile_d(7 downto 0);
         else
-          tile_d_r := tile_d_r(13 downto 8) & "00" &
-                      tile_d_r(5 downto 0) & "00";
+          tile_d_r := tile_d_r(3 downto 0) & "0000";
         end if;
       end if;
-      pel := tile_d_r(15 downto 14) & tile_d_r(7 downto 6);
+      pel := tile_d_r(7 downto 4);
       
       -- extract R,G,B from colour palette
       --attr_i := attr_d_r(1 downto 0) & tile_d_r(7) & attr_d_r(5 downto 2);
