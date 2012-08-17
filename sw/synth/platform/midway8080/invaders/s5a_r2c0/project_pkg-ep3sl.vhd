5ab48000fd8daff7857a67c87ad7ac4e5cb57fa1
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2012-08-17 07:45:18 +0000

    builds for 50Hz arcade monitors
    git-svn-id: https://svn.pacedev.net/repos/pace@1267 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/synth/platform/midway8080/invaders/s5a_r2c0/project_pkg-ep3sl.vhd
+++ sw/synth/platform/midway8080/invaders/s5a_r2c0/project_pkg-ep3sl.vhd
@@ -15,7 +15,7 @@ package project_pkg is
 	
   -- Reference clock is 24MHz
 	constant PACE_HAS_PLL								      : boolean := true;
-  constant PACE_HAS_FLASH                   : boolean := false;
+  --constant PACE_HAS_FLASH                   : boolean := false;
   --constant PACE_HAS_SRAM                    : boolean := false;
   constant PACE_HAS_SDRAM                   : boolean := false;
   constant PACE_HAS_SERIAL                  : boolean := false;
@@ -42,26 +42,26 @@ package project_pkg is
 --  constant PACE_VIDEO_H_SCALE       	      : integer := 2;
 --  constant PACE_VIDEO_V_SCALE       	      : integer := 2;
 
---  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_CVBS_720x288p_50Hz;
---  constant PACE_CLK0_DIVIDE_BY              : natural := 32;
---  constant PACE_CLK0_MULTIPLY_BY            : natural := 27;   	-- 24*27/32 = 20M25Hz
---  constant PACE_CLK1_DIVIDE_BY              : natural := 16;
---  constant PACE_CLK1_MULTIPLY_BY            : natural := 9;  		-- 24*9/16 = 13.5MHz
---  constant PACE_VIDEO_H_SCALE       	      : integer := 2;
---  constant PACE_VIDEO_V_SCALE       	      : integer := 1;
---  constant PACE_VIDEO_H_SYNC_POLARITY       : std_logic := '1';
---  constant PACE_VIDEO_V_SYNC_POLARITY       : std_logic := '1';
-  
-  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_1024x768_60Hz;
-  constant PACE_CLK0_DIVIDE_BY              : natural := 3;
-  constant PACE_CLK0_MULTIPLY_BY            : natural := 5;       -- 24*5/3 = 40MHz
-  constant PACE_CLK1_DIVIDE_BY              : natural := 24;
-  constant PACE_CLK1_MULTIPLY_BY            : natural := 65;  	  -- 24*65/24 = 65MHz
+  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_CVBS_720x288p_50Hz;
+  constant PACE_CLK0_DIVIDE_BY              : natural := 32;
+  constant PACE_CLK0_MULTIPLY_BY            : natural := 27;   	-- 24*27/32 = 20M25Hz
+  constant PACE_CLK1_DIVIDE_BY              : natural := 16;
+  constant PACE_CLK1_MULTIPLY_BY            : natural := 9;  		-- 24*9/16 = 13.5MHz
   constant PACE_VIDEO_H_SCALE       	      : integer := 2;
-  constant PACE_VIDEO_V_SCALE       	      : integer := 2;
+  constant PACE_VIDEO_V_SCALE       	      : integer := 1;
   constant PACE_VIDEO_H_SYNC_POLARITY       : std_logic := '1';
   constant PACE_VIDEO_V_SYNC_POLARITY       : std_logic := '1';
-
+  
+--  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_1024x768_60Hz;
+--  constant PACE_CLK0_DIVIDE_BY              : natural := 3;
+--  constant PACE_CLK0_MULTIPLY_BY            : natural := 5;       -- 24*5/3 = 40MHz
+--  constant PACE_CLK1_DIVIDE_BY              : natural := 24;
+--  constant PACE_CLK1_MULTIPLY_BY            : natural := 65;  	  -- 24*65/24 = 65MHz
+--  constant PACE_VIDEO_H_SCALE       	      : integer := 2;
+--  constant PACE_VIDEO_V_SCALE       	      : integer := 2;
+--  constant PACE_VIDEO_H_SYNC_POLARITY       : std_logic := '1';
+--  constant PACE_VIDEO_V_SYNC_POLARITY       : std_logic := '1';
+--
 --  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_1280x1024_60Hz;
 --  constant PACE_CLK0_DIVIDE_BY              : natural := 96;
 --  constant PACE_CLK0_MULTIPLY_BY            : natural := 157;     -- 24.675*157/96 = 40.192MHz
@@ -85,7 +85,17 @@ package project_pkg is
   constant S5AR2_EMULATED_SRAM_WIDTH_AD     : natural := 16;
   constant S5AR2_EMULATED_SRAM_WIDTH        : natural := 8;
 
+  constant S5AR2_EMULATED_FLASH_INIT_FILE   : string := "";
+  constant S5AR2_EMULATE_FLASH              : boolean := false;
+  constant S5AR2_EMULATED_FLASH_WIDTH_AD    : natural := 10;
+  constant S5AR2_EMULATED_FLASH_WIDTH       : natural := 8;
+
+  constant S5AR2_HAS_FLOPPY_IF              : boolean := false;
+
+  -- derived - do not edit
+  
   constant PACE_HAS_SRAM                    : boolean := S5AR2_EMULATE_SRAM;
+  constant PACE_HAS_FLASH                   : boolean := S5AR2_EMULATE_FLASH;
     
   --
 	-- Space Invaders-specific constants
@@ -94,8 +104,6 @@ package project_pkg is
   constant INVADERS_ROM_IN_FLASH            : boolean := PACE_HAS_FLASH;
 	constant INVADERS_USE_INTERNAL_WRAM				: boolean := not PACE_HAS_SRAM;
 
-	constant USE_VIDEO_VBLANK_INTERRUPT 			: boolean := false;
-	
   type from_PROJECT_IO_t is record
     not_used  : std_logic;
   end record;
