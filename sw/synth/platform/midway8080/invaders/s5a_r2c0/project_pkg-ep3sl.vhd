c0c7050215c02e85c805c84ad07468a654ac83b8
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2012-08-20 00:33:58 +0000

    broken
    git-svn-id: https://svn.pacedev.net/repos/pace@1271 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/synth/platform/midway8080/invaders/s5a_r2c0/project_pkg-ep3sl.vhd
+++ sw/synth/platform/midway8080/invaders/s5a_r2c0/project_pkg-ep3sl.vhd
@@ -26,13 +26,15 @@ package project_pkg is
 	--constant PACE_VIDEO_H_SIZE				        : integer := 224;
 	--constant PACE_VIDEO_V_SIZE				        : integer := 256; -- why not 240?
 
---  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_640x480_60Hz;
---  constant PACE_CLK0_DIVIDE_BY              : natural := 6;
---  constant PACE_CLK0_MULTIPLY_BY            : natural := 5;   -- 24*5/6 = 20MHz
---  constant PACE_CLK1_DIVIDE_BY              : natural := 19;
---  constant PACE_CLK1_MULTIPLY_BY            : natural := 20; 	-- 24*20/19 = 25.263158MHz
---	constant PACE_VIDEO_H_SCALE       	      : integer := 1;
---	constant PACE_VIDEO_V_SCALE       	      : integer := 1;
+  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_640x480_60Hz;
+  constant PACE_CLK0_DIVIDE_BY              : natural := 6;
+  constant PACE_CLK0_MULTIPLY_BY            : natural := 5;   -- 24*5/6 = 20MHz
+  constant PACE_CLK1_DIVIDE_BY              : natural := 19;
+  constant PACE_CLK1_MULTIPLY_BY            : natural := 20; 	-- 24*20/19 = 25.263158MHz
+	constant PACE_VIDEO_H_SCALE       	      : integer := 1;
+	constant PACE_VIDEO_V_SCALE       	      : integer := 1;
+  constant PACE_VIDEO_H_SYNC_POLARITY       : std_logic := '1';
+  constant PACE_VIDEO_V_SYNC_POLARITY       : std_logic := '1';
 
 --  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_800x600_60Hz;
 --  constant PACE_CLK0_DIVIDE_BY              : natural := 1;
@@ -42,6 +44,23 @@ package project_pkg is
 --  constant PACE_VIDEO_H_SCALE       	      : integer := 2;
 --  constant PACE_VIDEO_V_SCALE       	      : integer := 2;
 
+<<<<<<< .mine
+--  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_ARCADE_STD_336x240_60Hz;
+--  constant PACE_CLK0_DIVIDE_BY              : natural := 1;
+--  constant PACE_CLK0_MULTIPLY_BY            : natural := 1;   	-- 24*1/1 = 24MHz
+--  constant PACE_CLK1_DIVIDE_BY              : natural := 57;
+--  constant PACE_CLK1_MULTIPLY_BY            : natural := 17;  	-- 24*17/57 = 7.157895MHz
+--  constant PACE_VIDEO_H_SCALE       	      : integer := 1;
+--  constant PACE_VIDEO_V_SCALE       	      : integer := 1;
+--  constant PACE_VIDEO_H_SYNC_POLARITY       : std_logic := '0';
+--  constant PACE_VIDEO_V_SYNC_POLARITY       : std_logic := '0';
+  
+--  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_CVBS_720x288p_50Hz;
+--  constant PACE_CLK0_DIVIDE_BY              : natural := 32;
+--  constant PACE_CLK0_MULTIPLY_BY            : natural := 27;   	-- 24*27/32 = 20M25Hz
+--  constant PACE_CLK1_DIVIDE_BY              : natural := 16;
+--  constant PACE_CLK1_MULTIPLY_BY            : natural := 9;  		-- 24*9/16 = 13.5MHz
+=======
   constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_CVBS_720x288p_50Hz;
   constant PACE_CLK0_DIVIDE_BY              : natural := 32;
   constant PACE_CLK0_MULTIPLY_BY            : natural := 27;   	-- 24*27/32 = 20M25Hz
@@ -57,11 +76,26 @@ package project_pkg is
 --  constant PACE_CLK0_MULTIPLY_BY            : natural := 5;       -- 24*5/3 = 40MHz
 --  constant PACE_CLK1_DIVIDE_BY              : natural := 24;
 --  constant PACE_CLK1_MULTIPLY_BY            : natural := 65;  	  -- 24*65/24 = 65MHz
+>>>>>>> .r1269
+--  constant PACE_VIDEO_H_SCALE       	      : integer := 2;
+--  constant PACE_VIDEO_V_SCALE       	      : integer := 2;
+--  constant PACE_VIDEO_H_SYNC_POLARITY       : std_logic := '1';
+--  constant PACE_VIDEO_V_SYNC_POLARITY       : std_logic := '1';
+<<<<<<< .mine
+  
+--  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_1024x768_60Hz;
+--  constant PACE_CLK0_DIVIDE_BY              : natural := 3;
+--  constant PACE_CLK0_MULTIPLY_BY            : natural := 5;       -- 24*5/3 = 40MHz
+--  constant PACE_CLK1_DIVIDE_BY              : natural := 24;
+--  constant PACE_CLK1_MULTIPLY_BY            : natural := 65;  	  -- 24*65/24 = 65MHz
 --  constant PACE_VIDEO_H_SCALE       	      : integer := 2;
 --  constant PACE_VIDEO_V_SCALE       	      : integer := 2;
 --  constant PACE_VIDEO_H_SYNC_POLARITY       : std_logic := '1';
 --  constant PACE_VIDEO_V_SYNC_POLARITY       : std_logic := '1';
 --
+=======
+--
+>>>>>>> .r1269
 --  constant PACE_VIDEO_CONTROLLER_TYPE       : PACEVideoController_t := PACE_VIDEO_VGA_1280x1024_60Hz;
 --  constant PACE_CLK0_DIVIDE_BY              : natural := 96;
 --  constant PACE_CLK0_MULTIPLY_BY            : natural := 157;     -- 24.675*157/96 = 40.192MHz
@@ -85,6 +119,16 @@ package project_pkg is
   constant S5AR2_EMULATED_SRAM_WIDTH_AD     : natural := 16;
   constant S5AR2_EMULATED_SRAM_WIDTH        : natural := 8;
 
+<<<<<<< .mine
+  constant S5AR2_EMULATED_FLASH_INIT_FILE   : string := "";
+  constant S5AR2_EMULATE_FLASH              : boolean := false;
+  constant S5AR2_EMULATED_FLASH_WIDTH_AD    : natural := 10;
+  constant S5AR2_EMULATED_FLASH_WIDTH       : natural := 8;
+
+  constant S5AR2_HAS_FLOPPY_IF              : boolean := false;
+
+  constant PACE_HAS_FLASH                   : boolean := S5AR2_EMULATE_FLASH;
+=======
   constant S5AR2_EMULATED_FLASH_INIT_FILE   : string := "";
   constant S5AR2_EMULATE_FLASH              : boolean := false;
   constant S5AR2_EMULATED_FLASH_WIDTH_AD    : natural := 10;
@@ -94,6 +138,7 @@ package project_pkg is
 
   -- derived - do not edit
   
+>>>>>>> .r1269
   constant PACE_HAS_SRAM                    : boolean := S5AR2_EMULATE_SRAM;
   constant PACE_HAS_FLASH                   : boolean := S5AR2_EMULATE_FLASH;
     
