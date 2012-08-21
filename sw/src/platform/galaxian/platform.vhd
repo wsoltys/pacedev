8f781edc6617a5d138e5a9ab9704d482e2e0d440
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2012-08-21 00:15:25 +0000

    wip
    git-svn-id: https://svn.pacedev.net/repos/pace@1274 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/src/platform/galaxian/platform.vhd
+++ sw/src/platform/galaxian/platform.vhd
@@ -20,8 +20,7 @@ entity platform is
   port
   (
     -- clocking and reset
-    clk_i           : in std_logic_vector(0 to 3);
-    reset_i         : in std_logic;
+    clkrst_i        : in from_CLKRST_t;
 
     -- misc I/O
     buttons_i       : in from_BUTTONS_t;
@@ -41,11 +40,11 @@ entity platform is
     
     -- graphics
     
-    bitmap_i        : in from_BITMAP_CTL_t;
-    bitmap_o        : out to_BITMAP_CTL_t;
+    bitmap_i        : in from_BITMAP_CTL_a(1 to PACE_VIDEO_NUM_BITMAPS);
+    bitmap_o        : out to_BITMAP_CTL_a(1 to PACE_VIDEO_NUM_BITMAPS);
     
-    tilemap_i       : in from_TILEMAP_CTL_t;
-    tilemap_o       : out to_TILEMAP_CTL_t;
+    tilemap_i       : in from_TILEMAP_CTL_a(1 to PACE_VIDEO_NUM_TILEMAPS);
+    tilemap_o       : out to_TILEMAP_CTL_a(1 to PACE_VIDEO_NUM_TILEMAPS);
 
     sprite_reg_o    : out to_SPRITE_REG_t;
     sprite_i        : in from_SPRITE_CTL_t;
@@ -85,39 +84,39 @@ end platform;
 
 architecture SYN of platform is
 
-	alias clk_sys				  : std_logic is clk_i(0);
-	alias clk_video       : std_logic is clk_i(1);
+	alias clk_sys				  : std_logic is clkrst_i.clk(0);
+	alias clk_video       : std_logic is clkrst_i.clk(1);
 	
   -- uP signals  
   signal clk_3M_en			: std_logic;
   signal cpu_clk_en     : std_logic;
-  signal uP_addr        : std_logic_vector(15 downto 0);
-  signal uP_datai       : std_logic_vector(7 downto 0);
-  signal uP_datao       : std_logic_vector(7 downto 0);
-  signal uPmemwr        : std_logic;
-  signal uPnmireq       : std_logic;
+  signal cpu_a          : std_logic_vector(15 downto 0);
+  signal cpu_d_i        : std_logic_vector(7 downto 0);
+  signal cpu_d_o        : std_logic_vector(7 downto 0);
+  signal cpu_mem_wr     : std_logic;
+  signal cpu_nmireq     : std_logic;
 	                        
   -- ROM signals        
 	signal rom_cs					: std_logic;
-  signal rom_datao      : std_logic_vector(7 downto 0);
+  signal rom_d_o        : std_logic_vector(7 downto 0);
                         
   -- keyboard signals
 	                        
   -- VRAM signals       
 	signal vram_cs				: std_logic;
 	signal vram_wr				: std_logic;
-	signal vram_addr			: std_logic_vector(9 downto 0);
-  signal vram_datao     : std_logic_vector(7 downto 0);
+	signal vram_a			    : std_logic_vector(9 downto 0);
+  signal vram_d_o       : std_logic_vector(7 downto 0);
                         
   -- RAM signals        
   signal wram_cs        : std_logic;
   signal wram_wr        : std_logic;
-  signal wram_datao     : std_logic_vector(7 downto 0);
+  signal wram_d_o       : std_logic_vector(7 downto 0);
 
   -- RAM signals        
   signal cram_cs        : std_logic;
   signal cram_wr        : std_logic;
-	signal cram_datao		  : std_logic_vector(7 downto 0);
+	signal cram_d_o		    : std_logic_vector(7 downto 0);
 	
   -- interrupt signals
   signal nmiena_wr      : std_logic;
@@ -133,12 +132,12 @@ begin
 	GEN_EXTERNAL_WRAM : if not GALAXIAN_USE_INTERNAL_WRAM generate
 	
 	  -- SRAM signals (may or may not be used)
-	  sram_o.a <= std_logic_vector(resize(unsigned(uP_addr(13 downto 0)), sram_o.a'length));
-	  sram_o.d <= std_logic_vector(resize(unsigned(uP_datao), sram_o.d'length));
-		wram_datao <= sram_i.d(wram_datao'range);
+	  sram_o.a <= std_logic_vector(resize(unsigned(cpu_a(13 downto 0)), sram_o.a'length));
+	  sram_o.d <= std_logic_vector(resize(unsigned(cpu_d_o), sram_o.d'length));
+		wram_d_o <= sram_i.d(wram_d_o'range);
 		sram_o.be <= std_logic_vector(to_unsigned(1, sram_o.be'length));
 	  sram_o.cs <= '1';
-	  sram_o.oe <= wram_cs and not uPmemwr;
+	  sram_o.oe <= wram_cs and not cpu_mem_wr;
 	  sram_o.we <= wram_wr;
 
 	end generate GEN_EXTERNAL_WRAM;
@@ -148,45 +147,45 @@ begin
 	end generate GEN_NO_SRAM;
 	
   -- chip select logic
-  rom_cs <= '1' when uP_addr(15 downto 14) = "00" else '0';
-  wram_cs <= '1' when uP_addr(15 downto 11) = "01000" else '0';
-  vram_cs <= '1' when uP_addr(15 downto 11) = "01010" else '0';
-  cram_cs <= '1' when uP_addr(15 downto 10) = "010110" else '0';
-  inZero_cs <= '1' when uP_addr(15 downto 11) = "01100" else '0';
-  inOne_cs <= '1' when uP_addr(15 downto 11) = "01101" else '0';
-  dips_cs <= '1' when uP_addr(15 downto 11) = "01110" else '0';
+  rom_cs <= '1' when cpu_a(15 downto 14) = "00" else '0';
+  wram_cs <= '1' when cpu_a(15 downto 11) = "01000" else '0';
+  vram_cs <= '1' when cpu_a(15 downto 11) = "01010" else '0';
+  cram_cs <= '1' when cpu_a(15 downto 10) = "010110" else '0';
+  inZero_cs <= '1' when cpu_a(15 downto 11) = "01100" else '0';
+  inOne_cs <= '1' when cpu_a(15 downto 11) = "01101" else '0';
+  dips_cs <= '1' when cpu_a(15 downto 11) = "01110" else '0';
 
 	-- memory read mux
-	uP_datai <= rom_datao when rom_cs = '1' else
-							wram_datao when wram_cs = '1' else
-							vram_datao when vram_cs = '1' else
-							cram_datao when cram_cs = '1' else
+	cpu_d_i <= rom_d_o when rom_cs = '1' else
+							wram_d_o when wram_cs = '1' else
+							vram_d_o when vram_cs = '1' else
+							cram_d_o when cram_cs = '1' else
               inputs_i(0).d when inzero_cs = '1' else
               inputs_i(1).d when inone_cs = '1' else
               switches_i(7 downto 0) when dips_cs = '1' else
 							(others => 'X');
 	
-	vram_wr <= uPmemwr and vram_cs;
-	cram_wr <= cram_cs and uPmemwr;
-	wram_wr <= wram_cs and uPmemwr;
-  nmiena_wr <= uPmemwr when (uP_addr(15 downto 12) = "0111" and uP_addr(2 downto 0) = "001") else '0';
+	vram_wr <= cpu_mem_wr and vram_cs;
+	cram_wr <= cram_cs and cpu_mem_wr;
+	wram_wr <= wram_cs and cpu_mem_wr;
+  nmiena_wr <= cpu_mem_wr when (cpu_a(15 downto 12) = "0111" and cpu_a(2 downto 0) = "001") else '0';
 
   -- sprite registers
   sprite_reg_o.clk <= clk_sys;
   sprite_reg_o.clk_ena <= cpu_clk_en;
-  sprite_reg_o.a <= uP_addr(7 downto 0);
-  sprite_reg_o.d <= uP_datao;
-  sprite_reg_o.wr <=  uPmemwr when (uP_addr(15 downto 10) = "010110" and uP_addr(7 downto 6) = "01") 
+  sprite_reg_o.a <= cpu_a(7 downto 0);
+  sprite_reg_o.d <= cpu_d_o;
+  sprite_reg_o.wr <=  cpu_mem_wr when (cpu_a(15 downto 10) = "010110" and cpu_a(7 downto 6) = "01") 
                       else '0';
 
 	-- mangle tile address according to sprite layout
-	newTileAddr <=  tilemap_i.tile_a(11 downto 6) & tilemap_i.tile_a(4 downto 1) & 
-                  not tilemap_i.tile_a(5) & tilemap_i.tile_a(0);
+	newTileAddr <=  tilemap_i(1).tile_a(11 downto 6) & tilemap_i(1).tile_a(4 downto 1) & 
+                  not tilemap_i(1).tile_a(5) & tilemap_i(1).tile_a(0);
 
   -- unused outputs
 
   flash_o <= NULL_TO_FLASH;
-  bitmap_o <= NULL_TO_BITMAP_CTL;
+  bitmap_o <= (others => NULL_TO_BITMAP_CTL);
   sprite_o.ld <= '0';
   graphics_o <= NULL_TO_GRAPHICS;
   osd_o <= NULL_TO_OSD;
@@ -214,7 +213,7 @@ begin
     port map
     (
       clk				=> clk_sys,
-      reset			=> reset_i,
+      reset			=> clkrst_i.rst(0),
       clk_en		=> clk_3M_en
     );
   
@@ -226,21 +225,21 @@ begin
     (
       clk 		=> clk_sys,                                   
       clk_en	=> cpu_clk_en,
-      reset  	=> reset_i,                                     
+      reset  	=> clkrst_i.rst(0),                                     
 
-      addr   	=> uP_addr,
-      datai  	=> uP_datai,
-      datao  	=> uP_datao,
+      addr   	=> cpu_a,
+      datai  	=> cpu_d_i,
+      datao  	=> cpu_d_o,
 
       mem_rd 	=> open,
-      mem_wr 	=> uPmemwr,
+      mem_wr 	=> cpu_mem_wr,
       io_rd  	=> open,
       io_wr  	=> open,
 
       intreq 	=> '0',
-      intvec 	=> uP_datai,
+      intvec 	=> cpu_d_i,
       intack 	=> open,
-      nmi    	=> uPnmireq
+      nmi    	=> cpu_nmireq
     );
 
   interrupts_inst : entity work.Galaxian_Interrupts
@@ -251,23 +250,23 @@ begin
     port map
     (
       clk               => clk_sys,
-      reset             => reset_i,
+      reset             => clkrst_i.rst(0),
   
-      z80_data          => uP_datao,
+      z80_data          => cpu_d_o,
       nmiena_wr         => nmiena_wr,
   
 			vblank						=> graphics_i.vblank,
 			
       -- interrupt status & request lines
-      nmi_req           => uPnmireq
+      nmi_req           => cpu_nmireq
     );
 
 	rom_inst : entity work.galaxian_rom
 		port map
 		(
 			clock			=> clk_sys,
-			address		=> up_addr(13 downto 0),
-			q					=> rom_datao
+			address		=> cpu_a(13 downto 0),
+			q					=> rom_d_o
 		);
 	
 	-- wren_a *MUST* be GND for CYCLONEII_SAFE_WRITE=VERIFIED_SAFE
@@ -275,40 +274,40 @@ begin
 		port map
 		(
 			clock_b			=> clk_sys,
-			address_b		=> uP_addr(9 downto 0),
+			address_b		=> cpu_a(9 downto 0),
 			wren_b			=> vram_wr,
-			data_b			=> uP_datao,
-			q_b					=> vram_datao,
+			data_b			=> cpu_d_o,
+			q_b					=> vram_d_o,
 
 			clock_a			=> clk_video,
-			address_a		=> vram_addr,
+			address_a		=> vram_a,
 			wren_a			=> '0',
 			data_a			=> (others => 'X'),
-			q_a					=> tilemap_o.map_d(7 downto 0)
+			q_a					=> tilemap_o(1).map_d(7 downto 0)
 		);
-  tilemap_o.map_d(15 downto 8) <= (others => '0');
+  tilemap_o(1).map_d(15 downto 8) <= (others => '0');
 
 	vrammapper_inst : entity work.vramMapper
 		port map
 		(
 	    clk     => clk_video,
 
-	    inAddr  => tilemap_i.map_a(12 downto 0),
-	    outAddr => vram_addr
+	    inAddr  => tilemap_i(1).map_a(12 downto 0),
+	    outAddr => vram_a
 		);
 
 	cram_inst : entity work.galaxian_cram
 		port map
 		(
 			clock_b			=> clk_sys,
-			address_b		=> uP_addr(7 downto 0),
+			address_b		=> cpu_a(7 downto 0),
 			wren_b			=> cram_wr,
-			data_b			=> uP_datao,
-			q_b					=> cram_datao,
+			data_b			=> cpu_d_o,
+			q_b					=> cram_d_o,
 			
 			clock_a			=> clk_video,
-			address_a		=> tilemap_i.attr_a(7 downto 1),
-			q_a					=> tilemap_o.attr_d(15 downto 0)
+			address_a		=> tilemap_i(1).attr_a(7 downto 1),
+			q_a					=> tilemap_o(1).attr_d(15 downto 0)
 		);
 
 	gfxrom_inst : entity work.galaxian_gfxrom
@@ -316,7 +315,7 @@ begin
 		(
 			clock										=> clk_video,
 			address_a								=> newTileAddr,
-			q_a											=> tilemap_o.tile_d,
+			q_a											=> tilemap_o(1).tile_d(7 downto 0),
 			
 			address_b								=> sprite_i.a(9 downto 0),
 			q_b(31 downto 24)				=> sprite_o.d(7 downto 0),
@@ -336,10 +335,10 @@ begin
 				port map
 				(
 					clock				=> clk_sys,
-					address			=> uP_addr(10 downto 0),
-					data				=> up_datao,
+					address			=> cpu_a(10 downto 0),
+					data				=> cpu_d_o,
 					wren				=> wram_wr,
-					q						=> wram_datao
+					q						=> wram_d_o
 				);
 		
 		end generate GEN_INTERNAL_WRAM;
