1dacfdc6cfab2faedb30ba63173f577b48b7e370
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2014-04-16 08:28:30 +0000

    started on screen rendering routine (Z80)
    git-svn-id: https://svn.pacedev.net/repos/pace@1544 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/re/platform/appleii/loderun/loderun_z80.asm
+++ sw/re/platform/appleii/loderun/loderun_z80.asm
@@ -14,6 +14,12 @@ codebase		.equ		0x5200
 ; un-comment to pixel-double the display
 .define PIXEL_DOUBLE
 
+				; for 'trs80gp -ee' only
+				.macro BREAK
+				ld			a,#4
+				out			(0x47),a
+				.endm
+				
 				.macro GFXMOD, mode
 				ld			a,#mode
 				out			(131),a
@@ -76,6 +82,8 @@ codebase		.equ		0x5200
 
 display_title_screen: ; $6008
 				call		gcls1
+				xor			a
+				ld			(byte_A7),a
 				ld			hl,#title_data
 				xor			a
 				ld			d,a											; x count
@@ -117,13 +125,22 @@ display_title_screen: ; $6008
 				GFXMOD	0xB3										; 640x240, X-inc on write
 				jp			title_wait_for_key
 				
-; $6056
+loc_6056:	; $6056
 				xor			a
 ; store in zero page
 				ld			a,#5										; number of lives
 				ld			(no_lives),a				
+				ld			a,(byte_A7)
+				srl			a
+				jr			z,loc_6099
 ; do some crap
 				jp			display_title_screen
+
+loc_6099:	; $6099
+				call		loc_79AD
+loop:		jp			loop				
+				; other crap				
+				BREAK
 				
 title_wait_for_key: ; $618e
 ;				jsr			keybd_flush
@@ -142,15 +159,16 @@ title_wait_for_key: ; $618e
 				or			c
 				jr			nz,2$
 				pop			bc
-				djnz		1$
+				;djnz		1$											; *** FUDGE - shorten
+				ld			a,(byte_A7)
+				;bne
+				ld			a,#1
+				ld			(byte_A7),a
 ; do some other crap
-; test $A7
 				jp			loc_6056
 
 loc_61f6:
-; exit to debugger (trs80gp only)				
-break:	ld			a,#4
-				out			(0x47),a
+				BREAK
 
 gcls1: ; $7A51
 				ld			c,#240
@@ -166,6 +184,48 @@ gcls1: ; $7A51
 				jr			nz,1$
 				ret								
 
+loc_79AD:	; $79AD
+				call		gcls1
+;				call		gcls2
+				ld			b,#4										; 4 lines
+				ld			c,#220									; starting row
+1$:			push		bc				
+				ld			a,c
+				GFXY
+				xor			a
+				GFXX
+				ld			b,#35										; bytes/line
+.ifdef PIXEL_DOUBLE
+				ld			a,#0xCC									; pattern
+2$:			GFXDAT
+				GFXDAT
+.else
+				ld			a,#0xAA									; pattern
+2$:			GFXDAT
+.endif				
+				djnz		2$											; 1 line
+				pop			bc
+				inc			c
+				djnz		1$
+				ld			a,#0x10
+				ld			(row),a
+				ld			a,#0x00
+				ld			(col),a
+				call		display_message
+				
+				.asciz	"SCORE        MEN    LEVEL   "
+				
+				ret
+
+display_message:	; $86E0
+				pop			hl
+1$:			ld			a,(hl)
+				inc			hl
+				or			a
+				jr			nz,1$
+				push		hl
+				ret
+								
 read_paddles: ; $8702
 ; check for joystick buttons here
 				ld			a, #0xcb								; flag no paddles detected
--- sw/re/platform/appleii/loderun/zeropage.asm
+++ sw/re/platform/appleii/loderun/zeropage.asm
@@ -1,7 +1,12 @@
 ; zero-page variables
 
+col:							; $85
+				.db			0x00										; for display_message
+row:							; $86
+				.db			0x00										; for display_message
 paddles_detected: ; $95
 				.db			0xca										; paddles detected?
 no_lives:					; $98
 				.db			0x00										; number of lives
-																
+byte_A7:					; $A7
+				.db			0x00										; ???														
