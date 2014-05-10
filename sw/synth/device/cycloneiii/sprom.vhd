b3e697d6c34c4ba02f49419ebaf13d1d79ab6b2b
wsoltys <wiso@no.way>
2014-05-10 18:41:13 +0200

    some platforms still need numwords

--- sw/synth/device/cycloneiii/sprom.vhd
+++ sw/synth/device/cycloneiii/sprom.vhd
@@ -8,7 +8,7 @@ ENTITY sprom IS
 	GENERIC
 	(
 		init_file		  : string := "";
-		--numwords_a	  : natural;
+		numwords_a	  : natural;
 		widthad_a		  : natural;
 		width_a			  : natural := 8;
     outdata_reg_a : string := "UNREGISTERED"
