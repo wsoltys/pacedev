62d8167a0fa932d7d65720c87a98b0aaa1126926
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2015-09-24 13:22:10 +0000

    skeleton
    git-svn-id: https://svn.pacedev.net/repos/pace@1778 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/re/platform/zxspectrum/filmation/c/kl.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <sys/stat.h>
+#include <memory.h>
+
+#include <allegro.h>
+
+#define ALLEGRO_FULL_VERSION  ((ALLEGRO_VERSION << 4)|(ALLEGRO_SUB_VERSION))
+#if ALLEGRO_FULL_VERSION < 0x42
+  #define SS_TEXTOUT_CENTRE(s,f,str,w,h,c) \
+    textout_centre (s, f, str, w, h, c);
+#else
+  #define SS_TEXTOUT_CENTRE(s,f,str,w,h,c) \
+    textout_centre_ex(s, f, str, w, h, c, 0);
+#endif
+
+// pandora: run msys.bat and cd to this directory
+//          g++ kl.cpp -o kl -lallegro-4.4.2-md
+
+void print_text_single_colour ()
+{
+}
+
+void print_text_std_font (char *str)
+{
+}
+
+void print_text ()
+{
+}
+
+void main (int argc, char *argv[])
+{
+	allegro_init ();
+	install_keyboard ();
+
+	set_color_depth (8);
+	set_gfx_mode (GFX_AUTODETECT_WINDOWED, 256, 192, 0, 0);
+
+  // spectrum palette
+  PALETTE pal;
+  for (int c=0; c<16; c++)
+  {
+    pal[c].r = (c&(1<<1) ? ((c < 8) ? (0xCD>>2) : (0xFF>>2)) : 0x00);
+    pal[c].g = (c&(1<<2) ? ((c < 8) ? (0xCD>>2) : (0xFF>>2)) : 0x00);
+    pal[c].b = (c&(1<<0) ? ((c < 8) ? (0xCD>>2) : (0xFF>>2)) : 0x00);
+  }
+	set_palette_range (pal, 0, 15, 1);
+
+	clear_bitmap (screen);
+	const char *msg[] = 
+	{
+	  "THIS IS THE FONT FROM A MYSTERY GAME",
+	  "FROM A Z80 PLATFORM",
+	  "THAT I AM CURRENTLY EVALUATING FOR",
+	  "A POSSIBLE PORT TO",
+	  "THE COLOR COMPUTER 3",
+	  ""
+	};
+
+  for (unsigned m=0; *msg[m]; m++)
+  {
+    print_text_std_font ((char *)msg[m]);
+  }	
+  while (!key[KEY_ESC]);	  
+	while (key[KEY_ESC]);	  
+
+  allegro_exit ();
+}
+
+END_OF_MAIN();
