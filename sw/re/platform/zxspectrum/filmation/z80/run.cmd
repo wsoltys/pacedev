0c378618151f7ffec2a9758dcd6f2337bd83d7fa
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2016-01-02 10:16:22 +0000

    added TRS-80 emulator for testing
    git-svn-id: https://svn.pacedev.net/repos/pace@1914 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/re/platform/zxspectrum/filmation/z80/run.cmd
@@ -0,0 +1,34 @@
+@echo off
+set opt=""
+if "%2%" == "debug" set opt="-d"
+if "%2%" == "-d" set opt="-d"
+if "%1%" == "apple" goto apple
+if "%1%" == "apple2" goto apple
+if "%1%" == "coco" goto coco
+if "%1%" == "coco3" goto coco
+if "%1%" == "neogeo" goto neogeo
+if "%1%" == "trs80" goto trs80
+goto bad_sys
+
+:apple
+mess64d apple2e %2 -flop1 LODERUNNER.DSK
+goto end
+
+:coco
+if "%2%" == "video" set opt=-aviwrite coco.avi
+mess64d coco3 -snapsize 320x192 %opt% -flop1 LODERUN_6809.DSK
+goto end
+
+:neogeo
+echo neogeo not supported yet
+goto end
+
+:trs80
+trs80gp -m4 -na -ee kl_z80.cmd
+goto end
+
+:bad_sys
+echo Unknown system. Choose {apple^|coco^|neogeo^|trs80}
+goto end
+
+:end
