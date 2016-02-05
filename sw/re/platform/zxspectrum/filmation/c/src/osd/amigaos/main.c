ef2fd3d59acdbe1f2a9d0153705ead4500726154
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2016-02-05 01:17:22 +0000

    removed requirement for ixemul.library
    git-svn-id: https://svn.pacedev.net/repos/pace@1979 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/re/platform/zxspectrum/filmation/c/src/osd/amigaos/main.c
+++ sw/re/platform/zxspectrum/filmation/c/src/osd/amigaos/main.c
@@ -37,7 +37,7 @@ static struct IOStdReq *KeyIO;
 static struct MsgPort *KeyMP;
 static uint8_t *keyMatrix;
   
-const char __ver[40] = "$VER: Knight Lore v0.9a1 (04.02.2016)";
+const char __ver[40] = "$VER: Knight Lore v0.9a2 (05.02.2016)";
 
 void osd_delay (unsigned ms)
 {
