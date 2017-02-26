017cd0f6232a39c78ae14619d59c19ef4776f092
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2017-02-26 12:44:23 +0000

    more RE wip DVG 'emulator' displays character printing
    git-svn-id: https://svn.pacedev.net/repos/pace@2379 693b85d5-0402-4c10-a79c-edb74d372fca

--- sw/re/platform/asteroids/dvg.cpp
+++ sw/re/platform/asteroids/dvg.cpp
@@ -11,23 +11,45 @@ static void dvg_parse (uint8_t *buf)
   uint8_t *p = buf;
   bool done = false;
     
+  static char* chrtbl = " 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
   do
   {
     uint8_t cmd = *(p+1) >> 4;
     uint16_t offset = (p-buf);
-    
+    uint16_t x, y;
+    uint8_t s;
+    uint16_t a;
+    char chr;
+            
     switch (cmd)
     {
       // vec
       case 0x00 : case 0x01 : case 0x02 : case 0x03 :
       case 0x04 : case 0x05 : case 0x06 : case 0x07 :
       case 0x08 : case 0x09 :
-        printf ("$%04X: VEC\n", offset);
+        //   SSSS -mYY YYYY YYYY | BBBB -mXX XXXX XXXX
+        //   YYYY YYYY SSSS -mYY | XXXX XXXX BBBB -mXX 
+        x = *(p+2) + (((uint16_t)*(p+3) & 0x03) << 8);
+        y = *(p+0) + (((uint16_t)*(p+1) & 0x03) << 8);
+        printf ("$%04X: VEC (%02X%02X %02X%02X)\n", 
+                offset, *(p+1), *(p+0), *(p+3), *(p+2));
+        printf ("  (%c%d,%c%d)x%d,B=%d\n",
+                (*(p+3) & (1<<2) ? '-' : '+'), x,
+                (*(p+1) & (1<<2) ? '-' : '+'), y,
+                  cmd, *(p+3) >> 4);
         p += 4;
         break;
       // cur
       case 0x0A :
-        printf ("$%04X: CUR\n", offset);
+        // 1010 00yy yyyy yyyy | SSSS 00xx xxxx xxxx
+        // yyyy yyyy 1010 00yy | xxxx xxxx SSSS 00xx 
+        x = *(p+2) + (((uint16_t)*(p+3) & 0x03) << 8);
+        y = *(p+0) + (((uint16_t)*(p+1) & 0x03) << 8);
+        s = *(p+3) >> 4;
+        printf ("$%04X: CUR (%02X%02X %02X%02X)\n", 
+                offset, *(p+1), *(p+0), *(p+3), *(p+2));
+        printf ("  (%d,%d)x%d\n", x, y, s);
         p += 4;
         break;
       // halt
@@ -37,7 +59,25 @@ static void dvg_parse (uint8_t *buf)
         break;
       // jsr
       case 0x0C :
-        printf ("$%04X: JSR\n", offset);
+        // 1100 aaaa aaaa aaaa
+        // aaaa aaaa 1100 aaaa 
+        a = *(p+0) + (((uint16_t)*(p+1) & 0x0f) << 8);
+        printf ("$%04X: JSR (%02X%02X)\n", offset, *(p+1), *(p+0));
+        chr = '.';
+        // lookup function
+        {
+          unsigned i;
+          uint16_t base = 0x56d4;
+          for (i=0; i<32; i++)
+          {
+            if (*(p+0) == ram[base+2*i] && *(p+1) == ram[base+2*i+1])
+            {
+              chr = chrtbl[i];
+              break;
+            }
+          }
+        }
+        printf ("  addr=$%04X ('%c')\n", (a<<1)-0x800, chr);
         p += 2;
         break;
       // rts
@@ -47,7 +87,11 @@ static void dvg_parse (uint8_t *buf)
         break;
       // jmp
       case 0x0E :
-        printf ("$%04X: JMP\n", offset);
+        // 1110 aaaa aaaa aaaa
+        // aaaa aaaa 1110 aaaa 
+        a = *(p+0) + (((uint16_t)*(p+1) & 0x0f) << 8);
+        printf ("$%04X: JMP (%02X%02X)\n", offset, *(p+1), *(p+0));
+        printf ("  addr=$%04X\n", (a<<1));
         p += 2;
         break;
       // svec
@@ -76,4 +120,5 @@ int main (int argc, char *argv[])
 	fclose (fp);
 	
 	dvg_parse (&ram[0x4000]);
+	//dvg_parse (&ram[0x4402]);
 }
