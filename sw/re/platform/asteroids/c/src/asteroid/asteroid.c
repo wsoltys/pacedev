cdb5c7083dd3279ba0a3de22e57384b5e4a640c6
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2017-07-07 11:35:19 +0000

    interim checkin
    git-svn-id: https://svn.pacedev.net/repos/pace@2480 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/re/platform/asteroids/c/src/asteroid/asteroid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+//
+//  KNOWN BUGS:
+//
+// * (none)
+//
+//
+
+#define DBGPRINTF_FN    DBGPRINTF ("%s():\n", __FUNCTION__)
+//#define UNTESTED        DBGPRINTF ("*** %s(): UNTESTED ***\n", __FUNCTION__)
+#define UNTESTED        
+//#define UNIMPLEMENTED   DBGPRINTF ("*** %s(): UNMIPLEMENTED ***\n", __FUNCTION__)
+#define UNIMPLEMENTED   
+#define UNIMPLEMENTED_AUDIO
+
+#pragma pack(1)
+
+void asteroids2 (void)
+{
+}
---
+++ sw/re/platform/asteroids/c/src/osd/allegro/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <sys/stat.h>
+#include <memory.h>
+
+#include <allegro.h>
+
+#define ALLEGRO_FULL_VERSION  ((ALLEGRO_VERSION << 4)|(ALLEGRO_SUB_VERSION))
+#if ALLEGRO_FULL_VERSION < 0x42
+  #define SS_TEXTOUT_CENTRE(s,f,str,w,h,c) \
+    textout_centre (s, f, str, w, h, c);
+#else
+  #define SS_TEXTOUT_CENTRE(s,f,str,w,h,c) \
+    textout_centre_ex(s, f, str, w, h, c, 0);
+#endif
+
+// pandora: run msys.bat and cd to this directory
+//          g++ kl.c -o kl -lallegro-4.4.2-md
+
+// neogeo:  d:\mingw_something\setenv.bat
+//          g++ kl.c -o kl -lalleg
+
+void main (int argc, char *argv[])
+{
+	allegro_init ();
+	install_keyboard ();
+
+	set_color_depth (8);
+	set_gfx_mode (GFX_AUTODETECT_WINDOWED, 256, 192, 0, 0);
+
+	clear_bitmap (screen);
+  knight_lore (gfx);
+
+  while (!key[KEY_ESC]);	  
+	while (key[KEY_ESC]);	  
+
+  allegro_exit ();
+}
+
+END_OF_MAIN();
