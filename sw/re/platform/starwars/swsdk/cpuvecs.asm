6df5672d0e956abe7d6a7c578323dddea25b25bf
mark <mark@693b85d5-0402-4c10-a79c-edb74d372fca>
2017-09-08 12:32:58 +0000

    builds
    git-svn-id: https://svn.pacedev.net/repos/pace@2618 693b85d5-0402-4c10-a79c-edb74d372fca

---
+++ sw/re/platform/starwars/swsdk/cpuvecs.asm
@@ -0,0 +1,26 @@
+; Star Wars SDK
+;
+; 6809 CPU vectors
+
+				.list		(meb)										; macro expansion binary
+       	.area   _CODE (ABS,CON)
+				.module cpuvecs
+
+				.globl 	IRQ
+				.globl 	RESET
+
+				.org		0xFFE0
+
+NMI:
+				bra			NMI
+
+				.org		0xFFF0
+				
+				.dw			0x0C85					; reserved
+				.dw			NMI							; SWI3
+				.dw			NMI							; SWI2
+				.dw			NMI							; FIRQ
+				.dw			IRQ							; IRQ
+				.dw			NMI							; SWI1
+				.dw			NMI							; NMI
+				.dw			RESET						; RESET
